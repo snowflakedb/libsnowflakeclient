cc260d44b1b8b2f65e8788c775c38e3d0e134d5d
Shige Takeda <smtakeda@gmail.com>
2018-01-29 22:35:31 +0000

    fixed error handling

--- scripts/gen_gcov.sh
+++ scripts/gen_gcov.sh
@@ -2,7 +2,10 @@
 #
 # Generate Gcov files
 #
+set -o pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
 for f in $(ls ./lib); do
-    gcov --object-directory cmake-build/CMakeFiles/snowflakeclient.dir/lib/${f}.gcno lib/$f
+    gcov --object-directory ./cmake-build/CMakeFiles/snowflakeclient.dir/lib/${f}.gcno ./lib/$f
 done
