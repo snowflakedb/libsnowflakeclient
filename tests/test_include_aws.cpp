d043b9496b6330aca492389ba37ed526a6fe6909
notkriswagner <kristopher.wagner@snowflake.net>
2018-02-14 03:14:21 -0500

    Modifed AWS test to use cmocka.

--- tests/test_include_aws.cpp
+++ tests/test_include_aws.cpp
@@ -4,11 +4,20 @@
 
 
 #include <aws/core/Aws.h>
+#include "utils/test_setup.h"
 
 
-int main() {
+void test_include_aws(void **unused) {
     // Very basic test to see if library is properly linked
     Aws::SDKOptions options;
     Aws::InitAPI(options);
     Aws::ShutdownAPI(options);
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+      cmocka_unit_test(test_include_aws),
+    };
+    int ret = cmocka_run_group_tests(tests, NULL, NULL);
+    return ret;
 }
