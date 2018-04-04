4c6455ab69e44b7415a9cb895f99395b503a5eca
howryu <yuhaowei@umich.edu>
2018-04-04 13:56:22 -0700

    First commit

---
+++ cpp/FilePatternInterpreter.hpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#ifndef SNOWFLAKECLIENT_FILEPATTERNINTERPRETER_HPP
+#define SNOWFLAKECLIENT_FILEPATTERNINTERPRETER_HPP
+
+namespace Snowflake
+{
+namespace Client
+{
+class FilePatternInterpreter
+{
+
+};
+}
+}
+
+#endif //SNOWFLAKECLIENT_FILEPATTERNINTERPRETER_HPP
