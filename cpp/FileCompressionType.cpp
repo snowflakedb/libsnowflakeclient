6aa7e67b68ab81be8567c5afd009b482ffd3f595
howryu <yuhaowei@umich.edu>
2018-04-04 11:08:52 -0700

    Add auto detect for bz2 and zst

--- cpp/FileCompressionType.cpp
+++ cpp/FileCompressionType.cpp
@@ -9,17 +9,25 @@
 
 using Snowflake::Client::FileCompressionType;
 
+#define MAX_MAGIC_BYTES 4
+
 #define GZIP_MAGIC "\x1f\x8b"
 #define GZIP_NAME "gzip"
 #define GZIP_EXTENSION ".gz"
+#define DEFLATE_MAGIC_LOW "\x78\x01"
+#define DEFLATE_MAGIC_DEFAULT "\x78\x9c"
+#define DEFLATE_MAGIC_BEST "\x78\xda"
 #define DEFLATE_NAME "deflate"
 #define DEFLATE_EXTENSION ".deflate"
 #define RAW_DEFLATE_NAME "raw_deflate"
 #define RAW_DEFLATE_EXTENSION ".raw_deflate"
+#define BZIP2_MAGIC "\x42\x5a"
 #define BZIP2_NAME "bzip2"
 #define BZIP2_EXTENSION ".bz2"
+#define ZSTD_MAGIC "\x28\xb5\x2f\xfd"
 #define ZSTD_NAME "zstd"
 #define ZSTD_EXTENSION ".zst"
+#define BROTLI_MAGIC "\xce\xb2\xcf\x81"
 #define BROTLI_NAME "brotli"
 #define BROTLI_EXTENSION ".br"
 #define LZIP_NAME "lzip"
@@ -32,12 +40,22 @@ using Snowflake::Client::FileCompressionType;
 #define XZ_EXTENSION ".xz"
 #define COMPRESS_NAME "compress"
 #define COMPRESS_EXTENSION ".Z"
+#define PARQUET_MAGIC "\x50\x41\x52\x31"
 #define PARQUET_NAME "parquet"
 #define PARQUET_EXTENSION ".parquet"
+#define ORC_MAGIC "\x4f\x52\x43"
 #define ORC_NAME "orc"
 #define ORC_EXTENSION ".orc"
 
 std::vector<const char *> gzip_magics = {GZIP_MAGIC};
+std::vector<const char *> deflate_magics = {DEFLATE_MAGIC_LOW,
+                                            DEFLATE_MAGIC_DEFAULT,
+                                            DEFLATE_MAGIC_BEST};
+std::vector<const char *> bzip2_magics = {BZIP2_MAGIC};
+std::vector<const char *> orc_magics = {ORC_MAGIC};
+std::vector<const char *> parquet_magics = {PARQUET_MAGIC};
+std::vector<const char *> zstd_magics = {ZSTD_MAGIC};
+std::vector<const char *> brotli_magics = {BROTLI_MAGIC};
 
 Snowflake::Client::FileCompressionType::FileCompressionType(
   const char * fileExtension,
@@ -79,25 +97,25 @@ const FileCompressionType FileCompressionType::GZIP =
   FileCompressionType(GZIP_EXTENSION, GZIP_NAME, &gzip_magics, 2, true);
 
 const FileCompressionType FileCompressionType::DEFLATE =
-  FileCompressionType(DEFLATE_EXTENSION, DEFLATE_NAME, true);
+  FileCompressionType(DEFLATE_EXTENSION, DEFLATE_NAME, &deflate_magics, 2, true);
 
 const FileCompressionType FileCompressionType::RAW_DEFLATE=
   FileCompressionType(RAW_DEFLATE_EXTENSION, RAW_DEFLATE_NAME, true);
 
 const FileCompressionType FileCompressionType::BZIP2 =
-  FileCompressionType(BZIP2_EXTENSION, BZIP2_NAME, true);
+  FileCompressionType(BZIP2_EXTENSION, BZIP2_NAME, &bzip2_magics, 2, true);
 
 const FileCompressionType FileCompressionType::ZSTD =
-  FileCompressionType(ZSTD_EXTENSION, ZSTD_NAME, true);
+  FileCompressionType(ZSTD_EXTENSION, ZSTD_NAME, &zstd_magics, 4, true);
 
 const FileCompressionType FileCompressionType::BROTLI =
-  FileCompressionType(BROTLI_EXTENSION, BROTLI_NAME, true);
+  FileCompressionType(BROTLI_EXTENSION, BROTLI_NAME, &brotli_magics, 4, true);
 
 const FileCompressionType FileCompressionType::ORC =
-  FileCompressionType(ORC_EXTENSION, ORC_NAME, true);
+  FileCompressionType(ORC_EXTENSION, ORC_NAME, &orc_magics, 3, true);
 
 const FileCompressionType FileCompressionType::PARQUET =
-  FileCompressionType(PARQUET_EXTENSION, PARQUET_NAME, true);
+  FileCompressionType(PARQUET_EXTENSION, PARQUET_NAME, &parquet_magics, 4, true);
 
 const FileCompressionType FileCompressionType::LZIP =
   FileCompressionType(LZIP_EXTENSION, LZIP_NAME, false);
@@ -149,7 +167,7 @@ const char * FileCompressionType::getFileExtension() const
 const FileCompressionType *FileCompressionType::
   guessCompressionType(std::string &fileFullPath)
 {
-  char header[4] = {0};
+  char header[MAX_MAGIC_BYTES] = {0};
   std::ifstream srcFile(fileFullPath.c_str(), std::fstream::in | std::ios::binary);
   // read first 4 bytes to determine compression type
   srcFile.read(header, sizeof(header));
