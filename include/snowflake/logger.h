5f54d57df9017f496da5c3780c4ba28691e21496
Shige Takeda <smtakeda@gmail.com>
2018-04-04 02:23:29 +0000

    moved the log format pattern to the macro in the header

--- include/snowflake/logger.h
+++ include/snowflake/logger.h
@@ -14,6 +14,31 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * Log timestamp format.
+ *
+ * The parameters are:
+ * - Timestamp
+ * - Log Level
+ * - Namespace
+ * - Filename (excluding the directory)
+ * - Line number
+ */
+#define SF_LOG_TIMESTAMP_FORMAT "%s %-5s %-5s %-16s %4d: "
+
+/**
+ * Log timestamp format in Color
+ *
+ * The parameters are:
+ * - Timestamp
+ * - Log Level Color
+ * - Log Level
+ * - Namespace
+ * - Filename (excluding the directory)
+ * - Line number
+ */
+#define SF_LOG_TIMESTAMP_FORMAT_COLOR "%s %s%-5s\x1b[0m \x1b[90m%-5s %-16s %4d:\x1b[0m "
+
 typedef void (*log_LockFn)(void *udata, int lock);
 
 typedef enum SF_LOG_LEVEL {
@@ -49,8 +74,10 @@ void log_set_level(int level);
 
 void log_set_quiet(int enable);
 
-void log_log(int level, const char *file, int line, const char* ns, const char *fmt, ...);
+void
+log_log(int level, const char *file, int line, const char *ns, const char *fmt,
+        ...);
 
-SF_LOG_LEVEL log_from_str_to_level(const char* level_in_str);
+SF_LOG_LEVEL log_from_str_to_level(const char *level_in_str);
 
 #endif /* SNOWFLAKE_LOGGER_H */
