d7f81c5af3e5544a54160b00d33416fb11013f0a
howryu <yuhaowei@umich.edu>
2018-04-05 09:05:48 -0700

    Second commit

--- cpp/FilePatternInterpreter.cpp
+++ cpp/FilePatternInterpreter.cpp
@@ -3,3 +3,25 @@
  */
 
 #include "FilePatternInterpreter.hpp"
+#include <dirent.h>
+
+void Snowflake::Client::FilePatternInterpreter::populateSrcLocations(
+  std::string &sourceLocation)
+{
+  unsigned long dirSep = sourceLocation.find_last_of('/');
+  std::string dirPath = sourceLocation.substr(0, dirSep);
+  std::string filePattern = sourceLocation.substr(dirSep + 1);
+
+  DIR * dir = nullptr;
+  if ((dir = opendir(dirPath.c_str())) != NULL)
+  {
+     
+  }
+  else
+  {
+    // open dir failed
+    throw;
+  }
+
+
+}
