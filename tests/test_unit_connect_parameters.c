a3c446d426da593f615ff6fa5280fac7454c082f
Shige Takeda <smtakeda@gmail.com>
2018-04-05 00:15:26 +0000

    added region parameter and construct hostname from account and region if not set

---
+++ tests/test_unit_connect_parameters.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2018 Snowflake Computing, Inc. All rights reserved.
+ */
+#include <string.h>
+#include "utils/test_setup.h"
+#include "memory.h"
+
+/**
+ * Test default port
+ */
+void test_connection_parameters_default_port(void **unused) {
+    SF_CONNECT *sf = (SF_CONNECT *) SF_CALLOC(1, sizeof(SF_CONNECT));
+    sf->account = "testaccount";
+    sf->host = "testaccount.snowflakecomputing.com";
+    sf->user = "testuser";
+    sf->password = "testpassword";
+    assert_int_equal(
+        _snowflake_check_connection_parameters(sf), SF_STATUS_SUCCESS);
+    assert_string_equal(sf->port, "443");
+    SF_FREE(sf);
+}
+
+/**
+ * Test no host
+ */
+void test_connection_parameters_no_host(void **unused) {
+    SF_CONNECT *sf = (SF_CONNECT *) SF_CALLOC(1, sizeof(SF_CONNECT));
+    sf->account = "testaccount";
+    sf->user = "testuser";
+    sf->password = "testpassword";
+    assert_int_equal(
+        _snowflake_check_connection_parameters(sf), SF_STATUS_SUCCESS);
+    assert_string_equal(sf->host, "testaccount.snowflakecomputing.com");
+
+    SF_FREE(sf);
+}
+
+/**
+ * Test with region
+ */
+void test_connection_parameters_with_region(void **unused) {
+    SF_CONNECT *sf = (SF_CONNECT *) SF_CALLOC(1, sizeof(SF_CONNECT));
+    sf->account = "testaccount";
+    sf->user = "testuser";
+    sf->password = "testpassword";
+    sf->region = "somewhere";
+    assert_int_equal(
+        _snowflake_check_connection_parameters(sf), SF_STATUS_SUCCESS);
+    assert_string_equal(sf->host,
+                        "testaccount.somewhere.snowflakecomputing.com");
+
+    SF_FREE(sf);
+}
+
+/**
+ * Test account including region
+ */
+void test_connection_parameters_including_region(void **unused) {
+    SF_CONNECT *sf = (SF_CONNECT *) SF_CALLOC(1, sizeof(SF_CONNECT));
+
+    // allocate here, because it will be rewritten
+    sf->account = (char *) SF_CALLOC(1, 128);
+    strcpy(sf->account, "testaccount.somewhere");
+    sf->user = "testuser";
+    sf->password = "testpassword";
+    assert_int_equal(
+        _snowflake_check_connection_parameters(sf), SF_STATUS_SUCCESS);
+    assert_string_equal(sf->host,
+                        "testaccount.somewhere.snowflakecomputing.com");
+    assert_string_equal(sf->account, "testaccount");
+    assert_string_equal(sf->region, "somewhere");
+    SF_FREE(sf->account);
+    SF_FREE(sf);
+}
+
+/**
+ * Test account including region including dots
+ */
+void test_connection_parameters_including_region_including_dot(void **unused) {
+    SF_CONNECT *sf = (SF_CONNECT *) SF_CALLOC(1, sizeof(SF_CONNECT));
+
+    // allocate here, because it will be rewritten
+    sf->account = (char *) SF_CALLOC(1, 128);
+    strcpy(sf->account, "testaccount.somewhere.here.there");
+    sf->user = "testuser";
+    sf->password = "testpassword";
+    assert_int_equal(
+        _snowflake_check_connection_parameters(sf), SF_STATUS_SUCCESS);
+    assert_string_equal(sf->host,
+                        "testaccount.somewhere.here.there.snowflakecomputing.com");
+    assert_string_equal(sf->account, "testaccount");
+    assert_string_equal(sf->region, "somewhere.here.there");
+    SF_FREE(sf->account);
+    SF_FREE(sf);
+}
+
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_connection_parameters_default_port),
+        cmocka_unit_test(test_connection_parameters_no_host),
+        cmocka_unit_test(test_connection_parameters_with_region),
+        cmocka_unit_test(test_connection_parameters_including_region),
+        cmocka_unit_test(test_connection_parameters_including_region_including_dot),
+    };
+    int ret = cmocka_run_group_tests(tests, NULL, NULL);
+    return ret;
+}
