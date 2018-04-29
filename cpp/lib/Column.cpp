ac91740ec8fbc6f01e1077c9b45e1d9ab7da7a05
notkriswagner <kristopher.wagner@snowflake.net>
2018-04-28 20:08:41 -0400

    Added basic C++ Wrapper API. An example of how the API works is shown in "test_cpp_select1.cpp". Updated SF_TYPE to SF_DB_TYPE to solve naming conflict with Windows.

---
+++ cpp/lib/Column.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017-2018 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#include <snowflake/Column.hpp>
+
+Snowflake::Client::Column::Column(SF_COLUMN_DESC *column_desc_) {}
+
+Snowflake::Client::Column::~Column() {}
+
+int32 Snowflake::Client::Column::asInt32() {}
---
+++ tests/test_cpp_select1.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017-2018 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#include "utils/test_setup.h"
+#include "utils/TestSetup.hpp"
+
+void test_select1_cpp(void **unused) {
+    Snowflake::Client::Connection *cnx = TestSetup::connectionFactory();
+    SF_STATUS status;
+
+    try {
+        cnx->connect();
+        Snowflake::Client::Statement *stmt = new Snowflake::Client::Statement(*cnx);
+        std::string command("select 1;");
+        stmt->query(command);
+
+        int counter = 0;
+        while ((status = stmt->fetch()) == SF_STATUS_SUCCESS) {
+            assert_int_equal(stmt->column(1).asInt32(), 1);
+            ++counter;
+        }
+        //assert_int_equal(status, SF_STATUS_EOF);
+
+        delete stmt;
+        delete cnx;
+    } catch (...) /* Catch all error handler */ {
+        // Handle errors
+    }
+}
+
+int main(void) {
+    initialize_test(SF_BOOLEAN_FALSE);
+    const struct CMUnitTest tests[] = {
+      cmocka_unit_test(test_select1_cpp),
+    };
+    int ret = cmocka_run_group_tests(tests, NULL, NULL);
+    snowflake_global_term();
+    return ret;
+}
