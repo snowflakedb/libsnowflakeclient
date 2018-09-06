a9f1edf4d6c4265288fece91b0db4832d3d39fa9
notkriswagner <kristopher.wagner@snowflake.net>
2018-09-06 10:02:25 -0700

    Added warehouse option in connection setup function for SF test utils. Added print statements to test data setup so that we can debug what is going on in test runs.

--- scripts/sf_test_utils.py
+++ scripts/sf_test_utils.py
@@ -23,14 +23,17 @@ def init_connection_params():
         'database': os.getenv("SNOWFLAKE_TEST_DATABASE"),
         'role': os.getenv("SNOWFLAKE_TEST_ROLE"),
     }
-    host=os.getenv("SNOWFLAKE_TEST_HOST")
+    host = os.getenv("SNOWFLAKE_TEST_HOST")
     if host:
         params['host'] = host
-    port=os.getenv("SNOWFLAKE_TEST_PORT")
+    port = os.getenv("SNOWFLAKE_TEST_PORT")
     if port:
         params['port'] = port
-    protocol=os.getenv("SNOWFLAKE_TEST_PROTOCOL")
+    protocol = os.getenv("SNOWFLAKE_TEST_PROTOCOL")
     if protocol:
         params['protocol'] = protocol
+    warehouse = os.getenv("SNOWFLAKE_TEST_WAREHOUSE")
+    if warehouse:
+        params['warehouse'] = warehouse
 
     return params
