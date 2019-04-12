ad44e024e8442dfe674615ff21302e14b23e6734
notkriswagner <kristopher.wagner@snowflake.net>
2019-04-12 11:41:27 -0700

    Added file for mock functions

---
+++ tests/unit/mock/snowflake_mock.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2017-2019 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#ifndef SNOWFLAKECLIENT_SNOWFLAKE_MOCK_H
+#define SNOWFLAKECLIENT_SNOWFLAKE_MOCK_H
+
+#endif //SNOWFLAKECLIENT_SNOWFLAKE_MOCK_H
