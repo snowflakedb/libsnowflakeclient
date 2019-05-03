5494b7f2edb60d74496351029dba242dc4266fb0
viswanatha reddy <rvishureddy@gmail.com>
2019-05-02 23:26:53 -0700

    Azure Put: Exclude azure sdk and uuid packages from Mac OS X and Windows

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -16,5 +16,7 @@ source $DIR/build_openssl.sh -t $target
 source $DIR/build_curl.sh    -t $target
 source $DIR/build_awssdk.sh  -t $target
 source $DIR/build_cmocka.sh  -t Debug
-source $DIR/build_uuid.sh    -t $target
-$DIR/build_azuresdk.sh -t $target
+if [ "$(uname -s)" == "Linux" ] ; then
+  source $DIR/build_uuid.sh -t $target
+  $DIR/build_azuresdk.sh -t $target
+fi
