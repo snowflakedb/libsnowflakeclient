8bf68b079176363f3b4a067dbe4379c149de608d
Viswanatha Reddy <rvishureddy@gmail.com>
2019-11-08 11:59:11 -0800

    Build azure sdk with MT option to be compatible with all other

--- scripts/build_azuresdk.bat
+++ scripts/build_azuresdk.bat
@@ -6,7 +6,7 @@
 set platform=%1
 set build_type=%2
 set vs_version=%3
-set force_shared_crt=%4
+set build_with_md=%4
 
 set scriptdir=%~dp0
 call "%scriptdir%\_init.bat" %platform% %build_type% %vs_version%
@@ -30,7 +30,7 @@ set AZURE_CMAKE_BUILD_DIR=%AZURE_SOURCE_DIR%\cmake-build
 set AZURE_INSTALL_DIR=%scriptdir%\..\deps-build\%build_dir%\%vs_version%\azure
 set GIT_REPO="https://github.com/snowflakedb/azure-storage-cpplite.git"
 set CLONE_CMD="git clone -b master $GIT_REPO $AZURE_SOURCE_DIR"
-set VERSION="v0.1.15"
+set VERSION="v0.1.16"
 set GIT=git.exe
 
 if exist %AZURE_SOURCE_DIR% rmdir /S /Q %AZURE_SOURCE_DIR%
@@ -53,7 +53,7 @@ cmake %AZURE_SOURCE_DIR% ^
 -G %generator% ^
 -DCMAKE_BUILD_TYPE=%build_type% ^
 -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF ^
--DFORCE_SHARED_CRT=%force_shared_crt% ^
+-DBUILD_WITH_MD=%build_with_md% ^
 -DBUILD_SHARED_LIBS=OFF ^
 -DUSE_OPENSSL=true ^
 -DCURL_LINK_TYPE=static ^
