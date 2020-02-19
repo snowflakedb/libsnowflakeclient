01621071767a24775f2e5b25929c1f40b9605d77
Shige Takeda <shige.takeda@snowflake.com>
2020-02-19 10:53:59 -0800

    set parameters secret

--- ci/scripts/set_parameters.bat
+++ ci/scripts/set_parameters.bat
@@ -1,18 +1,21 @@
 setlocal
-@echo on
+@echo off
 set scriptdir=%~dp0
 
 if /I "%CLOUD_PROVIDER%"=="AWS" (
+    echo == AWS
     gpg --quiet --batch --yes --decrypt --passphrase="%PARAMETERS_SECRET%" ^
       --output %scriptdir%..\..\parameters.json ^
       %scriptdir%..\..\.github\workflows\parameters_aws_capi.json.gpg
 )
 if /I "%CLOUD_PROVIDER%"=="AZURE" (
+    echo == AZURE
     gpg --quiet --batch --yes --decrypt --passphrase="%PARAMETERS_SECRET%" ^
       --output %scriptdir%..\..\parameters.json ^
       %scriptdir%..\..\.github\workflows\parameters_azure_capi.json.gpg
 )
 if /I "%CLOUD_PROVIDER%"=="GCP" (
+    echo === GCP
     gpg --quiet --batch --yes --decrypt --passphrase="%PARAMETERS_SECRET%" ^
       --output %scriptdir%..\..\parameters.json ^
       %scriptdir%..\..\.github\workflows\parameters_gcp_capi.json.gpg
