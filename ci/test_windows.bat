89a3d60cb6423c0836fe7a3f8930d8636fc5d32b
Shige Takeda <shige.takeda@snowflake.com>
2020-02-18 20:47:58 -0800

    added test run

---
+++ ci/test_windows.bat
@@ -0,0 +1,18 @@
+::
+:: Test libsnowflake
+::
+@echo on
+setlocal
+python --version
+
+set platform=%1
+set build_type=%2
+set vs_version=%3
+
+call "%scriptdir%_init.bat" %platform% %build_type% %vs_version%
+@echo on
+
+set cmake_dir=cmake-build-%arcdir%-%vs_version%-%build_type%
+
+%cmake_dir%\tests\%build_type%\test_unit_logger
+%cmake_dir%\tests\%build_type%\test_unit_connect_parameters.exe
