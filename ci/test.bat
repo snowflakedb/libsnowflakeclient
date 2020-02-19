c9e7d01d1bee96daefbff62ab1807d735b471577
Shige Takeda <shige.takeda@snowflake.com>
2020-02-19 09:07:21 -0800

    fix quote

--- ci/test.bat
+++ ci/test.bat
@@ -4,9 +4,10 @@
 
 @echo on
 setlocal
+echo == %GITHUB_ACTIONS%
 if not defined GITHUB_ACTIONS (
-    set path=C:\Program Files\7-Zip;%path%
-    set path=C:\Python37;C:\python37\scripts;%path%
+    set "path=C:\Program Files\7-Zip;%path%"
+    set "path=C:\Python37;C:\python37\scripts;%path%"
 )
 set scriptdir=%~dp0
 set curdir=%cd%
