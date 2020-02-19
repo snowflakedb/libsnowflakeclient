e325ef9322b762f4f3112030d34c50775b7b0b4e
ankit-bhatnagar167 <ankit.bhatnagar@snowflake.com>
2020-02-18 17:15:02 -0800

    Remove safe fail

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Run C API tests
 #
@@ -8,7 +8,7 @@ function usage() {
     exit 2
 }
 
-set -o pipefail
+#set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
