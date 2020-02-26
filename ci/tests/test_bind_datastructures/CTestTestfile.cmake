8f6f1e9014a95630489cc9f6dbedda954a2aec47
ankit-bhatnagar167 <ankit.bhatnagar@snowflake.com>
2020-02-26 14:16:54 -0800

    Removed cmake folder

--- ci/tests/test_bind_datastructures/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/host/tests/test_bind_datastructures
-# Build directory: /mnt/host/cmake-build/tests/test_bind_datastructures
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(unit_tests)
