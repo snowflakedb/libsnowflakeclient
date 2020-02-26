ab76702edbf6d5e9446f68b34df2bb50ef71a8fc
ankit-bhatnagar167 <ankit.bhatnagar@snowflake.com>
2020-02-26 14:11:28 -0800

    Removed cmake folder

--- ci/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/host
-# Build directory: /mnt/host/cmake-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(examples)
-subdirs(tests)
