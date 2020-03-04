53b85e0dacfbcd9be1e87d706887019c63a21657
Shige Takeda <shige.takeda@snowflake.com>
2020-03-04 03:29:50 +0000

    changed docker image

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -6,18 +6,16 @@ set -o pipefail
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $THIS_DIR/_init.sh
 
-export GIT_COMMIT=`git rev-parse HEAD`
-libsnowflake_ver='0.4.5'
+[[ -z "$BUILD_TYPE" ]] && echo "Specify BUILD_TYPE. [Debug, Release]" && exit 1
 
-echo $THIS_DIR
-
+BUILD_IMAGE_NAME="${BUILD_IMAGE_NAMES[$DRIVER_NAME-centos6-default]}"
+echo $BUILD_IMAGE_NAME
 docker pull "${BUILD_IMAGE_NAME}"
-
 docker run \
         -t \
         -v $(cd $THIS_DIR/.. && pwd):/mnt/host \
         -v $WORKSPACE:/mnt/workspace \
-        -e BUILD_TYPE=Debug \
+        -e BUILD_TYPE=$BUILD_TYPE \
         -e LOCAL_USER_ID=$(id -u $USER) \
         -e AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY \
--- ci/test_linux.sh
+++ ci/test_linux.sh
@@ -10,13 +10,15 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $THIS_DIR/_init.sh
 CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker pull "${TEST_IMAGE_NAME}"
+[[ -z "$BUILD_TYPE" ]] && echo "Specify BUILD_TYPE. [Debug, Release]" && exit 1
 
+TEST_IMAGE_NAME="${TEST_IMAGE_NAMES[$DRIVER_NAME-centos6-default]}"
+docker pull "${TEST_IMAGE_NAME}"
 docker run \
         -t \
         -v $(cd $THIS_DIR/.. && pwd):/mnt/host \
         -v $WORKSPACE:/mnt/workspace \
-        -e BUILD_TYPE=Debug \
+        -e BUILD_TYPE=$BUILD_TYPE \
         -e JOB_NAME \
         -e BUILD_NUMBER \
         -e LOCAL_USER_ID=$(id -u $USER) \
@@ -24,5 +26,5 @@ docker run \
         -e AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY \
         -w /mnt/host \
-        "${BUILD_IMAGE_NAME}" \
+        "${TEST_IMAGE_NAME}" \
         "ci/test/test.sh"
