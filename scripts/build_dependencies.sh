ef4fdd35874c6095b63801128770f93782d0334f
Kris Wagner <32375658+sfc-gh-kwagner@users.noreply.github.com>
2020-05-29 13:25:57 -0700

    Update build_dependencies.sh

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -13,6 +13,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target=Debug
 source $DIR/_init.sh $@
 source $DIR/build_openssl.sh -t $target
+source $DIR/build_oob.sh     -t $target
 source $DIR/build_curl.sh    -t $target
 source $DIR/build_awssdk.sh  -t $target
 source $DIR/build_cmocka.sh  -t Debug
