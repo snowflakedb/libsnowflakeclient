6301b7810aea3cf3001a0955fe8877d59078a002
notkriswagner <kristopher.wagner@snowflake.net>
2020-05-29 13:56:14 -0700

    Updated build order for dependencies script

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -14,10 +14,10 @@ target=Debug
 source $DIR/_init.sh $@
 source $DIR/build_openssl.sh -t $target
 source $DIR/build_oob.sh     -t $target
-source $DIR/build_curl.sh    -t $target
-source $DIR/build_awssdk.sh  -t $target
-source $DIR/build_cmocka.sh  -t Debug
 if [[ "$PLATFORM" == "linux" ]]; then
 	source $DIR/build_uuid.sh -t $target
 fi
+source $DIR/build_curl.sh    -t $target
+source $DIR/build_awssdk.sh  -t $target
+source $DIR/build_cmocka.sh  -t Debug
 source $DIR/build_azuresdk.sh -t $target
