6bf819285445dabdebc74b55dbbeedb393bbdb4e
Viswanatha Reddy <viswanatha.reddy@snowflake.com>
2020-07-01 12:07:43 -0700

    Now test jenkins failure for get

--- tests/test_simple_put.cpp
+++ tests/test_simple_put.cpp
@@ -731,7 +731,7 @@ int main(void) {
   }
 
   const struct CMUnitTest tests[] = {
-    /*cmocka_unit_test_teardown(test_simple_put_auto_compress, teardown),
+    cmocka_unit_test_teardown(test_simple_put_auto_compress, teardown),
     cmocka_unit_test_teardown(test_simple_put_auto_detect_gzip, teardown),
     cmocka_unit_test_teardown(test_simple_put_no_compress, teardown),
     cmocka_unit_test_teardown(test_simple_put_gzip, teardown),
@@ -746,7 +746,7 @@ int main(void) {
     cmocka_unit_test_teardown(test_large_get, donothing),
     cmocka_unit_test_teardown(test_large_reupload, donothing),
     cmocka_unit_test_teardown(test_verify_upload, teardown),
-    cmocka_unit_test_teardown(test_large_put_threshold, teardown),*/
+    cmocka_unit_test_teardown(test_large_put_threshold, teardown),
     cmocka_unit_test_teardown(test_simple_put_uploadfail, teardown)
   };
   int ret = cmocka_run_group_tests(tests, gr_setup, gr_teardown);
