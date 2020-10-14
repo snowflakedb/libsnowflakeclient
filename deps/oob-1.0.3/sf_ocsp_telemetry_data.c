0413ed6453a01f7589fd948cc40faf62a32f5109
sfc-gh-hchaturvedi <harsh.chaturvedi@snowflake.com>
2020-10-14 20:31:33 +0000

    Move ocsp_telemetry files to oob telemetry folder

--- deps/oob-1.0.3/sf_ocsp_telemetry_data.c
+++ deps/oob-1.0.3/sf_ocsp_telemetry_data.c
@@ -2,7 +2,11 @@
 * Copyright (c) 2018-2020 Snowflake Computing
 */
 
-#include "sf_ocsp_telemetrty_data.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "sf_ocsp_telemetry_data.h"
 
 SF_OTD *get_ocsp_telemetry_instance()
 {
@@ -11,87 +15,87 @@ SF_OTD *get_ocsp_telemetry_instance()
 
 void sf_otd_set_event_type(char *event_type, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->event_type,OCSP_TELEMETRY_EVENT_MAX_LEN, event_type));
+  snprintf(ocsp_telemetry_data->event_type,OCSP_TELEMETRY_EVENT_MAX_LEN, event_type);
 }
 
 void sf_otd_set_event_sub_type(char *event_sub_type, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  if (strlen(ocsp_telemetry_data) > 0)
+  if (strlen(ocsp_telemetry_data->event_sub_type) > 0)
   {
     strncat(ocsp_telemetry_data->event_sub_type, " | ",
-        OCSP_RESPONSE_SUB_EVENT_MAX_LEN-strlen(ocsp_telemetry_data->event_sub_type));
+        OCSP_TELEMETRY_SUB_EVENT_MAX_LEN-strlen(ocsp_telemetry_data->event_sub_type));
     strncat(ocsp_telemetry_data->event_sub_type, event_sub_type,
-            OCSP_RESPONSE_SUB_EVENT_MAX_LEN-strlen(ocsp_telemetry_data->event_sub_type));
+            OCSP_TELEMETRY_SUB_EVENT_MAX_LEN-strlen(ocsp_telemetry_data->event_sub_type));
   }
   else
   {
-    snprintf(ocsp_telemetry_data->event_sub_type,OCSP_TELEMETRY_SUB_EVENT_MAX_LEN, event_sub_type));
+    snprintf(ocsp_telemetry_data->event_sub_type,OCSP_TELEMETRY_SUB_EVENT_MAX_LEN, event_sub_type);
   }
 }
 
 void sf_otd_set_sfc_peer_host(char *sfc_peer_host, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->sfc_peer_host, OCSP_TELEMETRY_HOSTNAME_MAX_LEN, sfc_peer_host));
+  snprintf(ocsp_telemetry_data->sfc_peer_host, OCSP_TELEMETRY_HOSTNAME_MAX_LEN, sfc_peer_host);
 }
 
 void sf_otd_set_certid(char *certid, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->certid,OCSP_TELEMETRY_CERTID_MAX_LEN, certid));
+  snprintf(ocsp_telemetry_data->cert_id,OCSP_TELEMETRY_CERTID_MAX_LEN, certid);
 }
 
 void sf_otd_set_ocsp_request(char *ocsp_req_b64, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->ocsp_req_b64,OCSP_TELEMETRY_REQUEST_MAX_LEN, ocsp_req_b64));
+  snprintf(ocsp_telemetry_data->ocsp_req_b64,OCSP_TELEMETRY_REQUEST_MAX_LEN, ocsp_req_b64);
 }
 
-void sf_otd_set_event_sub_type(char *ocsp_responder_url, SF_OTD* ocsp_telemetry_data)
+void sf_otd_set_ocsp_responder_url(char *ocsp_responder_url, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->ocsp_responder_url,OCSP_TELEMETRY_OCSP_URL_MAX_LEN, ocsp_responder_url));
+  snprintf(ocsp_telemetry_data->ocsp_responder_url,OCSP_TELEMETRY_OCSP_URL_MAX_LEN, ocsp_responder_url);
 }
 
 void sf_otd_set_error_msg(char *error_msg, SF_OTD* ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
 
-  snprintf(ocsp_telemetry_data->error_msg,OCSP_TELEMETRY_ERROR_MSG_MAX_LEN, error_msg));
+  snprintf(ocsp_telemetry_data->error_msg,OCSP_TELEMETRY_ERROR_MSG_MAX_LEN, error_msg);
 }
 
 void sf_otd_set_insecure_mode(int insecure_mode, SF_OTD *ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
@@ -101,7 +105,7 @@ void sf_otd_set_insecure_mode(int insecure_mode, SF_OTD *ocsp_telemetry_data)
 
 void sf_otd_set_fail_open_mode(int failopen_mode, SF_OTD *ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
@@ -111,7 +115,7 @@ void sf_otd_set_fail_open_mode(int failopen_mode, SF_OTD *ocsp_telemetry_data)
 
 void sf_otd_set_cache_hit(int cache_hit, SF_OTD *ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
@@ -121,7 +125,7 @@ void sf_otd_set_cache_hit(int cache_hit, SF_OTD *ocsp_telemetry_data)
 
 void sf_otd_set_cache_enabled(int cache_enabled, SF_OTD *ocsp_telemetry_data)
 {
-  if (ocsp_telemetry_data == null)
+  if (!ocsp_telemetry_data)
   {
     return;
   }
