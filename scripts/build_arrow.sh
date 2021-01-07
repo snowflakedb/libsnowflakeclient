524fb7c6a28579df53a5f96df862a8746f1bbeed
Julian Lou <jlou@simba.com>
2021-01-07 09:54:02 -0800

    Fix incorrect relative path in Arrow build script.

--- scripts/build_arrow.sh
+++ scripts/build_arrow.sh
@@ -11,7 +11,7 @@ function usage() {
 }
 set -o pipefail
 
-#Change the version in the arrow.mk to build 
+#Change the version in the arrow.mk to build
 ARROW_VERSION=0.17.0
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -28,20 +28,20 @@ GIT_REPO="https://github.com/apache/arrow.git"
 CLONE_CMD="git clone -b master $GIT_REPO $ARROW_SOURCE_DIR"
 
 if [ ! -d $ARROW_SOURCE_DIR ]; then
-  n=0 
+  n=0
   # retry 5 times on cloning
-  until [ $n -ge 5 ] 
+  until [ $n -ge 5 ]
   do
     if $CLONE_CMD ; then
       break
-    fi  
+    fi
     n=$[$n+1]
   done
 
   if [ ! -d $ARROW_SOURCE_DIR ]; then
     echo "[Error] failed to clone repo from $GIT_REPO"
     exit 1
-  fi  
+  fi
 
   cd $ARROW_SOURCE_DIR
   git checkout tags/apache-arrow-$ARROW_VERSION -b apache-arrow-$ARROW_VERSION || true
@@ -52,8 +52,8 @@ else
 fi
 
 if [[ "$PLATFORM" == "linux" ]]; then
-    cp $THIS_DIR/arrow.mk ../deps/
-    cd ../deps/
+    cp $THIS_DIR/arrow.mk $THIS_DIR/../deps/
+    cd $THIS_DIR/../deps/
     BUILD_TYPE=$BUILD_TYPE CC=gcc52 CXX=g++52 make -f arrow.mk
 else
     echo "[ERROR] $PLATFORM is not supported"
