a2decfcbe253b721224375475015a2c76b519a1a
SimbaGithub <48035983+SimbaGithub@users.noreply.github.com>
2021-02-25 21:38:48 -0800

    Add SecretDetector to mask secret information in log (#260)
    * Add SecretDetector to mask secret information in log
    
    * Fix build issue with Windows Debug
    
    * Fix build issue on Windows Debug 64bit
    
    * Bug fix for escape characters
    
    * fix merge mistake
    
    Co-authored-by: Harry Xi <harryx@simba.com>

--- cpp/logger/SFLogger.cpp
+++ cpp/logger/SFLogger.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "SFLogger.hpp"
+#include "SecretDetector.hpp"
 
 Snowflake::Client::ISFLogger *
   Snowflake::Client::SFLogger::m_externalLogger = nullptr;
@@ -15,4 +16,47 @@ void Snowflake::Client::SFLogger::init(ISFLogger *logger)
 Snowflake::Client::ISFLogger * Snowflake::Client::SFLogger::getExternalLogger()
 {
   return m_externalLogger;
+}
+
+void log_masked_va_list(FILE* fp, const char *fmt, va_list args)
+{
+  std::string maskedMsg = Snowflake::Client::SFLogger::getMaskedMsgVA(fmt, args);
+  fprintf(fp, maskedMsg.c_str());
+}
+
+std::string Snowflake::Client::SFLogger::getMaskedMsg(const char* fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  std::string maskedMsg = getMaskedMsgVA(fmt, args);
+  va_end(args);
+
+  return maskedMsg;
+}
+
+std::string Snowflake::Client::SFLogger::getMaskedMsgVA(const char* fmt, va_list args)
+{
+  size_t bufLen = 4096;
+  // Just in case not to fall into dead loop. 8MB would be large enough for
+  // single log.
+  const int MAX_LOG_LEN = 8 * 1024 * 1024;
+  std::vector<char> buf(bufLen);
+
+  int ret = -1;
+  while ((ret < 0) && (bufLen <= MAX_LOG_LEN))
+  {
+    // va_list can only be consumed once. Make a copy here in case need to retry
+    // with larger buffer size.
+    va_list copy;
+    va_copy(copy, args);
+    ret = sb_vsnprintf(buf.data(), bufLen, bufLen - 1, fmt, copy);
+    va_end(copy);
+    if (ret < 0)
+    {
+      bufLen *= 2;
+      buf.resize(bufLen);
+    }
+  }
+
+  return SecretDetector::maskSecrets(std::string(buf.data()));
 }
--- cpp/logger/SFLogger.hpp
+++ cpp/logger/SFLogger.hpp
@@ -7,6 +7,7 @@
 
 #include "snowflake/ISFLogger.hpp"
 #include "snowflake/logger.h"
+#include <string>
 
 namespace Snowflake
 {
@@ -20,13 +21,18 @@ public:
 
   static ISFLogger * getExternalLogger();
 
+  static std::string getMaskedMsg(const char* fmt, ...);
+
+  static std::string getMaskedMsgVA(const char* fmt, va_list args);
+
 private:
   static ISFLogger * m_externalLogger;
 
 #define CXX_LOG_FATAL(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_FATAL, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_FATAL, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_fatal(CXX_LOG_NS, __VA_ARGS__); \
   } \
@@ -34,7 +40,8 @@ private:
 #define CXX_LOG_ERROR(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_ERROR, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_ERROR, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_error(CXX_LOG_NS, __VA_ARGS__); \
   } \
@@ -42,7 +49,8 @@ private:
 #define CXX_LOG_WARN(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_WARN, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_WARN, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_warn(CXX_LOG_NS, __VA_ARGS__); \
   } \
@@ -50,7 +58,8 @@ private:
 #define CXX_LOG_INFO(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_INFO, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_INFO, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_info(CXX_LOG_NS, __VA_ARGS__); \
   } \
@@ -58,7 +67,8 @@ private:
 #define CXX_LOG_DEBUG(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_DEBUG, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_DEBUG, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_debug(CXX_LOG_NS, __VA_ARGS__); \
   } \
@@ -66,7 +76,8 @@ private:
 #define CXX_LOG_TRACE(...)  \
   if (SFLogger::getExternalLogger() != NULL) \
   { \
-    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_TRACE, __FILE__, __VA_ARGS__); \
+    SFLogger::getExternalLogger()->logLine(SF_LOG_LEVEL::SF_LOG_TRACE, __FILE__, \
+                                           SFLogger::getMaskedMsg(__VA_ARGS__).c_str()); \
   } else { \
     sf_log_trace(CXX_LOG_NS, __VA_ARGS__); \
   } \
