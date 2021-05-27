7536478a124b9b2af558f5bf33d5a1fb24a1d5b1
Harry Xi <harryx@simba.com>
2021-05-27 16:22:28 -0700

    debug for boost build

--- scripts/build_boost_linux.sh
+++ scripts/build_boost_linux.sh
@@ -45,6 +45,8 @@ fi
 
 cd $BOOST_SOURCE_DIR
 echo "using gcc : : $CXX ; " >> tools/build/src/user-config.jam
+cp ../build.sh ./tools/build/src/engine/
+chmod +x ./tools/build/src/engine/build.sh
 echo CXX=$CXX ./bootstrap.sh --prefix=. --with-toolset=gcc --with-libraries=filesystem,regex,system
 CXX=$CXX ./bootstrap.sh --prefix=. --with-toolset=gcc --with-libraries=filesystem,regex,system
 echo ./b2 stage --stagedir=$BOOST_BUILD_DIR --includedir=$BOOST_BUILD_DIR/include toolset=gcc variant=$VARIANT link=static address-model=64 cflags="-Wall -m64 -D_REENTRANT -DCLUNIX -fPIC -O3" -a install
