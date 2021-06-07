9ea61f15c33385305e1c4b105956759d9241143f
Harry Xi <harryx@simba.com>
2021-06-07 14:31:04 -0700

    temporarily remove building boost from source for debugging

--- scripts/build_arrow.sh
+++ scripts/build_arrow.sh
@@ -20,12 +20,6 @@ source $DIR/_init.sh $@
 
 [[ -n "$GET_VERSION" ]] && echo $ARROW_VERSION && exit 0
 
-if [[ "$PLATFORM" == "linux" ]]; then
-    source $DIR/build_boost_linux.sh -t $target
-    source $DIR/build_arrow_linux.sh -t $target
-    exit 0
-fi
-
 cd $DIR/../deps-build
 if [ -d "arrow" ]; then rm -rf arrow; fi
 if [ -d "arrow_deps" ]; then rm -rf arrow_deps; fi
@@ -38,3 +32,8 @@ if [ -d "arrow" ]; then mv arrow $DEPENDENCY_DIR; fi
 if [ -d "arrow_deps" ]; then mv arrow_deps $DEPENDENCY_DIR; fi
 if [ -d "boost" ]; then mv boost $DEPENDENCY_DIR; fi
 
+cd $DIR
+if [[ "$PLATFORM" == "linux" ]]; then
+    source $DIR/build_arrow_linux.sh -t $target
+    exit 0
+fi
