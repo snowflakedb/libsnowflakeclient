570b034eb5cddf426b78c5006ea136ad7a9d36e2
Weike Dai <weike.dai@snowflake.com>
2021-06-22 19:10:57 +0000

    test miss code

--- lib/connection.c
+++ lib/connection.c
@@ -324,10 +324,10 @@ sf_bool STDCALL curl_post_call(SF_CONNECT *sf,
             // Error is set in the perform function
             break;
         }
-        if ((json_error = json_copy_string_no_alloc(query_code, *json, "code",
-                                                    QUERYCODE_LEN)) !=
-            SF_JSON_ERROR_NONE &&
-            json_error != SF_JSON_ERROR_ITEM_NULL) {
+        if (SF_BOOLEAN_TRUE) {
+            json_error = json_copy_string_no_alloc(query_code, *json, "code",
+                                                    QUERYCODE_LEN);
+            log_warn("Query code missing: %s", *json);
             JSON_ERROR_MSG(json_error, error_msg, "Query code");
             SET_SNOWFLAKE_ERROR(error, SF_STATUS_ERROR_BAD_JSON, error_msg,
                                 SF_SQLSTATE_UNABLE_TO_CONNECT);
