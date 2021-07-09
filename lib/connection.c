ae82fe90eaba23b011f59c7a3f82d895023db926
Weike Dai <weike.dai@snowflake.com>
2021-07-09 20:24:16 +0000

    minor change

--- lib/connection.c
+++ lib/connection.c
@@ -425,7 +425,7 @@ sf_bool STDCALL curl_post_call(SF_CONNECT *sf,
                                     SF_SQLSTATE_UNABLE_TO_CONNECT);
                 break;
             }
-            if(sf->log_spec_info){
+            if(sf->log_query_exec_steps_info){
                 if(counter_for_code == 1000){
                     cJSON *newJson = snowflake_cJSON_Duplicate(*json, cJSON_True);
                     const char* del = "rowset";
@@ -445,7 +445,7 @@ sf_bool STDCALL curl_post_call(SF_CONNECT *sf,
         }
 
         ret = SF_BOOLEAN_TRUE;
-        if(sf->log_spec_info){
+        if(sf->log_query_exec_steps_info){
             if(counter == 1000){
                 log_info("curl post call loop, query code : %s", query_code);
                 counter = 0;
