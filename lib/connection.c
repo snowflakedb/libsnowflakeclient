c09248d93d42e14247b4b4c5c6e8fa7a5b783bf0
Weike Dai <weike.dai@snowflake.com>
2021-07-13 16:23:29 +0000

    test finish

--- lib/connection.c
+++ lib/connection.c
@@ -426,7 +426,7 @@ sf_bool STDCALL curl_post_call(SF_CONNECT *sf,
                 break;
             }
             if(sf->log_query_exec_steps_info){
-                if(counter_for_code == 1){
+                if(counter_for_code == 1000){
                     cJSON *newJson = snowflake_cJSON_Duplicate(*json, cJSON_True);
                     const char* del = "rowset";
                     //delete the sensitive information in case it leaks to customer
