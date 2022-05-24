e6caaac3e74be551530c0408aab80b8fbaf81947
Harry Xi <harry.xi@insightsoftware.com>
2022-05-24 14:58:53 -0700

    disable build arrow from source on x86 Linux/Mac

--- ci/build_win.bat
+++ ci/build_win.bat
@@ -53,11 +53,11 @@ goto :EOF
     if %ERRORLEVEL% NEQ 0 goto :error
     call :download_build_component cmocka "%cmocka_build_script%" "%dynamic_runtime%"
     if %ERRORLEVEL% NEQ 0 goto :error
+    call :build_component arrow "%arrow_build_script%"
+    if %ERRORLEVEL% NEQ 0 goto :error
     if defined GITHUB_ACTIONS (
         rd /S /Q %scriptdir%\..\deps
     )
-    call :build_component arrow "%arrow_build_script%"
-    if %ERRORLEVEL% NEQ 0 goto :error
     call :build_component libsnowflakeclient "%libsnowflakeclient_build_script%" "%dynamic_runtime%"
     if %ERRORLEVEL% NEQ 0 goto :error
     if defined GITHUB_ACTIONS (
