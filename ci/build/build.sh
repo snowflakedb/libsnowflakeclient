940d2db1bbc7c4a84206bf365497595d17662db8
sfc-gh-kdama <kirankumar.dama@snowflake.com>
2022-06-24 14:16:33 -0700

    Upload Artifacts

--- ci/build/build.sh
+++ ci/build/build.sh
@@ -41,12 +41,8 @@ function download_build_component()
             if ! aws s3 cp --only-show-errors $src_path/$zip_file_name $ARTIFACTS_DIR; then
                 echo "=== build: $component_name ==="
                 "$component_script" -t "$build_type"
-                if [[ "$GIT_BRANCH" == "origin/master" ]]; then
-                    echo "=== upload $component_name"
-                    upload_to_sfc_dev1_data $component_name $component_version $build_type
-                else
-                    echo "No upload $component_name"
-                fi
+                echo "=== upload $component_name"
+                upload_to_sfc_dev1_data $component_name $component_version $build_type
             else
                 echo "=== download and extract: $component_name"
                 pushd $DEPENDENCY_DIR >& /dev/null
