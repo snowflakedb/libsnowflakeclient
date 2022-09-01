6e52608e4c5964809e8be05eef39be06f4316448
Harry Xi <harry.xi@insightsoftware.com>
2022-09-01 14:54:42 -0700

    update openssl 1.1.1q

--- scripts/build_openssl.bat
+++ scripts/build_openssl.bat
@@ -9,7 +9,7 @@
 :: - vs14 / vs15
 
 @echo off
-set OPENSSL_VERSION=1.1.1n
+set OPENSSL_VERSION=1.1.1q
 call %*
 goto :EOF
 
@@ -65,7 +65,7 @@ call "%scriptdir%utils.bat" :setup_visual_studio %vs_version%
 echo === building openssl: %curdir%\..\deps\%OPENSSL_DIR%
 cd "%scriptdir%..\deps
 rd /s /q %OPENSSL_DIR%
-7z x openssl-1.1.1n.zip
+7z x openssl-1.1.1q.zip
 cd "%scriptdir%..\deps\%OPENSSL_DIR%"
 echo === %PERL_EXE% Configure %openssl_debug_option% %openssl_target% no-shared
 %PERL_EXE% Configure %openssl_debug_option% %openssl_target% no-shared
