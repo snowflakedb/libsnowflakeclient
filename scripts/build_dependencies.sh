0d4b6a63ebfc15c517b836e57c26f274bf5c4b49
Tanner Sansbury <tsansbury-rmn@users.noreply.github.com>
2023-02-03 14:17:22 -0600

    build(fix): build dependencies before dependents

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -12,12 +12,14 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target=Debug
 source $DIR/_init.sh $@
-source $DIR/build_openssl.sh -t $target
-source $DIR/build_curl.sh    -t $target
-source $DIR/build_awssdk.sh  -t $target
-source $DIR/build_arrow.sh  -t $target
-source $DIR/build_cmocka.sh  -t Debug
+source $DIR/build_zlib.sh -t $target
 if [[ "$PLATFORM" == "linux" ]]; then
-	source $DIR/build_uuid.sh -t $target
+    source $DIR/build_uuid.sh -t $target
 fi
+source $DIR/build_openssl.sh -t $target
+source $DIR/build_oob.sh -t $target
+source $DIR/build_curl.sh -t $target
+source $DIR/build_awssdk.sh -t $target
 source $DIR/build_azuresdk.sh -t $target
+source $DIR/build_cmocka.sh -t Debug
+source $DIR/build_arrow.sh -t $target
