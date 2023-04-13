df5d2ef991c08533621e336401efa3a4b9bad925
Shane Scott <95254122+sfc-gh-sscott@users.noreply.github.com>
2023-04-13 11:11:55 -0700

    Update S3 bucket from sfc-jenkins to sfc-eng-jenkins (#491)

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -5,10 +5,10 @@ UTILS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PLATFORM_ARCH=$(uname -p)
 if [[ "$PLATFORM_ARCH" == "x86_64" ]] || [[ "$PLATFORM_ARCH" == "i386" ]]; then
-  export REP_URL_PREFIX="s3://sfc-jenkins/repository"
+  export REP_URL_PREFIX="s3://sfc-eng-jenkins/repository"
   export DEP_URL_PREFIX="s3://sfc-dev1-data/dependency"
 else
-  export REP_URL_PREFIX="s3://sfc-jenkins/repository-$PLATFORM_ARCH"
+  export REP_URL_PREFIX="s3://sfc-eng-jenkins/repository-$PLATFORM_ARCH"
   export DEP_URL_PREFIX="s3://sfc-dev1-data/dependency-$PLATFORM_ARCH"
 fi
 
