eaabf37602bb30e2992ef12f8e093b895860f456
sfc-gh-ext-simba-hx <harry.xi@insightsoftware.com>
2023-06-16 11:29:23 -0700

    fix curl build hang issue on Mac

--- scripts/build_curl.sh
+++ scripts/build_curl.sh
@@ -82,7 +82,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
     # build
     if [[ "$ARCH" == "universal" ]]; then
         echo "[INFO] Building Universal Binary"
-        make distclean clean &> /dev/null || true
+        make clean &> /dev/null || true
         export CFLAGS="-arch x86_64 -Xarch_x86_64 -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
@@ -90,7 +90,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         make > /dev/null
         make install /dev/null
 
-        make distclean clean &> /dev/null || true
+        make clean &> /dev/null || true
         export CFLAGS="-arch arm64 -Xarch_arm64 -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
@@ -101,7 +101,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         mv $LIBCURL_BUILD_DIR/lib/../libcurl.a $LIBCURL_BUILD_DIR/lib/libcurl.a
     elif [[ "$ARCH" == "x86" ]]; then
         echo "[INFO] Building x86 Binary"
-        make distclean clean &> /dev/null || true
+        make clean &> /dev/null || true
         export CFLAGS="-arch i386 -Xarch_i386 -DSIZEOF_LONG_INT=4 -Xarch_i386 -DHAVE_LONG_LONG -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
@@ -110,7 +110,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         make install /dev/null
     elif [[ "$ARCH" == "x64" ]]; then
         echo "[INFO] Building x64 Binary"
-        make distclean clean &> /dev/null || true
+        make clean &> /dev/null || true
         export CFLAGS="-arch x86_64 -Xarch_x86_64 -DSIZEOF_LONG_INT=8 -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
@@ -119,7 +119,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         make install /dev/null
     else
         echo "[INFO] Building $ARCH Binary"
-        make distclean clean &> /dev/null || true
+        make clean &> /dev/null || true
         export CFLAGS="-arch $ARCH -DSIZEOF_LONG_INT=8 -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
