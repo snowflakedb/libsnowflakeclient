48ae57dbac0a6f2ec381ba764a2b83d308628f92
Joyce Ling <115662568+sfc-gh-ext-simba-jl@users.noreply.github.com>
2023-08-02 23:27:53 -0700

    debug

--- scripts/gen_lcov.sh
+++ scripts/gen_lcov.sh
@@ -6,10 +6,10 @@
 
 CMAKE_DIR=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
+cd $DIR
 
 echo "=== debug gen_lcov.sh: $DIR"
-working_dir=$PWD/libsnowflakeclient/$CMAKE_DIR/CMakeFiles/snowflakeclient.dir/
+working_dir=$PWD/$CMAKE_DIR/CMakeFiles/snowflakeclient.dir/
 if [ -e $working_dir ]; then
     find $working_dir
     echo "=== done: $working_dir"
@@ -18,7 +18,7 @@ else
 fi
 echo "=== debug gen_lcov.sh ends"
 
-lcov -c -d $PWD/libsnowflakeclient/$CMAKE_DIR/CMakeFiles/snowflakeclient.dir/ --output-file coverage_unfiltered.info
+lcov -c -d ./$CMAKE_DIR/CMakeFiles/snowflakeclient.dir/ --output-file coverage_unfiltered.info
 
 lcov --remove coverage_unfiltered.info -o coverage.info \
     '/usr/*' \
@@ -28,3 +28,13 @@ lcov --remove coverage_unfiltered.info -o coverage.info \
 genhtml coverage.info --output-directory libsfc_coverage_report
 
 rm coverage_unfiltered.info
+
+echo "=== debug gen_lcov.sh: current directory: $PWD"
+FILE=$DIR/coverage.info
+if [ -f "$FILE" ]; then
+    echo "=== debug: $DIR/coverage.info exists"
+else
+    echo "=== debug: $DIR/coverage.info does not exist"
+    find $DIR
+    echo "=== done: $DIR"
+fi
