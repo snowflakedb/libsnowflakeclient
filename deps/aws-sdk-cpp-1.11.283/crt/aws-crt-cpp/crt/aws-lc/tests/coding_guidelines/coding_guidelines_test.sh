2728589caf0b9b0789710af87dd63674cd5f532d
sfc-gh-ext-simba-hx <harry.xi@insightsoftware.com>
2024-03-14 03:05:00 +0000

    remove unnecessary code

--- deps/aws-sdk-cpp-1.11.283/crt/aws-crt-cpp/crt/aws-lc/tests/coding_guidelines/coding_guidelines_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0 OR ISC
-
-./tests/coding_guidelines/style.sh
-./tests/coding_guidelines/c99_gcc_test.sh
