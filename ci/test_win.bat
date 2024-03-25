3c33fd2c428d2f5e8c24226a05c5765024814408
sfc-gh-ext-simba-hx <harry.xi@insightsoftware.com>
2024-03-25 14:32:33 -0700

    revert tem changes

--- ci/test_win.bat
+++ ci/test_win.bat
@@ -73,8 +73,8 @@ exit /b 0
         if !ERRORLEVEL! NEQ 0 goto :error
     )
     pushd %cmake_dir%
-        ::temporary set to wrong one to see if this actually work
-        set APPVEYOR_BUILD_FOLDER=%scriptdir%..\tests
+        :: test cases would need this to find data files
+        set APPVEYOR_BUILD_FOLDER=%scriptdir%..
         ctest -V -E "valgrind.*"
         if %ERRORLEVEL% NEQ 0 (
             call :drop_schema
