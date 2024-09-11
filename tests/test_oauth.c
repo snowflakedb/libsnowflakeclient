fa55e11afbb38c4ecf31e6e64bb81aa9e4bf854c
John Yun <140559986+sfc-gh-ext-simba-jy@users.noreply.github.com>
2024-09-11 15:48:43 -0700

    remove

--- tests/test_oauth.c
+++ tests/test_oauth.c
@@ -27,8 +27,7 @@ void test_connect_with_oauth(void **unused) {
     if (protocol) {
         snowflake_set_attribute(sf, SF_CON_PROTOCOL, protocol);
     }
-    char* token = "ver:1-hint:92019684051950-ETMsDgAAAZHjCazwABRBRVMvQ0JDL1BLQ1M1UGFkZGluZwEAABAAEDLFbJVGI/VDGAIG0Jm0AQsAAABQPbcQJdbDk+Q0Zpk/0x9WWvPtfvqRPrRXnRqpRTvo4doyr2GRi3szvTSB0ARuep3UxDnkvtFyYVPL8KrDxocJG7PqGqdOzghtGIh3XEs2sR0AFPax6y9x7O+Kn8I2WE4PjRx+Ynjm";
-    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "oauth");
+    char* token = "<Pass your token here>";
     snowflake_set_attribute(sf, SF_CON_OAUTH_TOKEN, token);
 
     SF_STATUS status = snowflake_connect(sf);
