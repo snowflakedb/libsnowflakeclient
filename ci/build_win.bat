169063c00267af97a1ddb67a70a855f444601c56
norrislee <norris.lee@insightsoftware.com>
2024-10-29 10:50:54 -0700

    Remove accidentally added files

--- ci/build_win.bat
+++ ci/build_win.bat
@@ -26,7 +26,6 @@ set aws_build_script="%scriptdir%..\scripts\build_awssdk.bat"
 set azure_build_script="%scriptdir%..\scripts\build_azuresdk.bat"
 set cmocka_build_script="%scriptdir%..\scripts\build_cmocka.bat"
 set arrow_build_script="%scriptdir%..\scripts\build_arrow.bat"
-set picojson_build_script="%scriptdir%..\scripts\build_picojson.bat"
 set libsnowflakeclient_build_script="%scriptdir%..\scripts\build_libsnowflakeclient.bat"
 
 set upload_artifact_script="%scriptdir%container\upload_artifact.bat"
@@ -59,8 +58,6 @@ goto :EOF
     call :download_build_component cmocka "%cmocka_build_script%" "%dynamic_runtime%"
     if %ERRORLEVEL% NEQ 0 goto :error
     call :download_build_component arrow "%arrow_build_script%" "%dynamic_runtime%"
-    if %ERRORLEVEL% NEQ 0 goto :error
-    call :download_build_component picojson "%picojson_build_script%" "%dynamic_runtime%"
     if %ERRORLEVEL% NEQ 0 goto :error
     if defined GITHUB_ACTIONS (
         rd /S /Q %scriptdir%\..\deps
