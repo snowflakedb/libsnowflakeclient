c966297eaf805084c815281a8118bbdd5b75d305
sfc-gh-ext-simba-hx <harry.xi@insightsoftware.com>
2025-02-18 15:56:50 -0800

    temporarily enable debugging

--- tests/test_lob.c
+++ tests/test_lob.c
@@ -423,7 +423,8 @@ void test_lob_describe_only_json(void **unused)
 }
 
 int main(void) {
-    initialize_test(SF_BOOLEAN_FALSE);
+    // temporarily enable debugging
+    initialize_test(SF_BOOLEAN_TRUE);
     initialize_lob_data();
     const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_lob_retrieval_arrow),
