c691aba8b74b5fad7aba34a4789459949aec5233
John Yun <jonghun.yun@insightsoftware.com>
2025-02-19 16:09:57 -0800

    fix error after merging master

--- lib/snowflake_util.h
+++ lib/snowflake_util.h
@@ -11,29 +11,6 @@ extern "C" {
  * Validate str ends with the suffix
  */
 sf_bool ends_with(char* str, char* suffix);
-
-	// trim from end (in place)
-	static inline void rtrim(std::string& s, char c)
-	{
-		s.erase(std::find_if(s.rbegin(), s.rend(), [c](char ch) {
-			return ch != c;
-			}).base(), s.end());
-	}
-
-	// trim from beginning (in place)
-	static inline void ltrim(std::string& s, char c)
-	{
-		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [c](char ch) {
-			return ch != c;
-			}));
-	}
-
-	// trim from both ends (in place)
-	static inline void trim(std::string& s, char c)
-	{
-		ltrim(s, c);
-		rtrim(s, c);
-	}
 #ifdef __cplusplus
 }
 #endif
