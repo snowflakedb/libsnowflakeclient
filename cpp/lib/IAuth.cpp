1f7eac91505aaf5923ea36cb1916f4821565dcc3
John Yun <jonghun.yun@insightsoftware.com>
2025-02-25 10:03:49 -0800

    copy from SNOW-715619v2

--- cpp/lib/IAuth.cpp
+++ cpp/lib/IAuth.cpp
@@ -3,11 +3,31 @@
  */
 
 #include <string>
+#include <regex>
+#ifdef _WIN32
+#include <WS2tcpip.h>
+#else
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
 #include "cJSON.h"
 #include "../include/snowflake/entities.hpp"
 #include "../logger/SFLogger.hpp"
 #include "snowflake/IAuth.hpp"
 #include "../../lib/authenticator.h"
+
+#include <openssl/rand.h>
+#include "../include/snowflake/entities.hpp"
+#include "../logger/SFLogger.hpp"
+
+#ifdef __APPLE__
+#include <CoreFoundation/CFBundle.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <ApplicationServices/ApplicationServices.h>
+#endif
 
 namespace Snowflake
 {
@@ -76,6 +96,253 @@ namespace Client
 
             return url;
         }
+
+        int IAuthenticatorExternalBrowser::getPort()
+        {
+            return m_authWebServer->getPort();
+        }
+
+        /**
+         * Authenticate user by external browser
+         */
+        void IAuthenticatorExternalBrowser::authenticate()
+        {
+#ifdef _WIN32
+            m_authWinSock = AuthWinSock();
+            if (m_authWinSock.isError())
+            {
+                return;
+            }
+#endif
+            m_authWebServer->start();
+            if (m_authWebServer->isError())
+            {
+                return;
+            }
+
+            std::map<std::string, std::string> out;
+            getLoginUrl(out, m_authWebServer->getPort());
+            if (isError())
+            {
+                return;
+            }
+
+            startWebBrowser(out[std::string("LOGIN_URL")]);
+            if (isError())
+            {
+                return;
+            }
+            m_proofKey = out[std::string("PROOF_KEY")];
+
+            m_authWebServer->setTimeout(m_browser_response_timeout);
+            m_authWebServer->startAccept();
+            if (m_authWebServer->isError())
+            {
+                return;
+            }
+            // accept SAML token
+            while (m_authWebServer->receive())
+            {
+                // nop
+            }
+
+            m_authWebServer->stop();
+            if (m_authWebServer->isError())
+            {
+                return;
+            }
+
+            m_token = m_authWebServer->getSAMLToken();
+            m_consentCacheIdToken = m_authWebServer->isConsentCacheIdToken();
+        }
+
+        /**
+         * Update data map to get final authentication.
+         * @param dataMap data map for the final authentication
+         */
+        void IAuthenticatorExternalBrowser::updateDataMap(jsonObject_t& dataMap)
+        {
+            dataMap["PROOF_KEY"] = picojson::value(m_proofKey);
+            dataMap["TOKEN"] = picojson::value(m_token);
+            dataMap["AUTHENTICATOR"] = picojson::value(SF_AUTHENTICATOR_EXTERNAL_BROWSER);
+        }
+
+        /**
+         * Get Login URL for multiple SAML
+         * @param out the login URL
+         * @param port port number listening to get SAML token
+         */
+        void IAuthenticatorExternalBrowser::getLoginUrl(
+            std::map<std::string, std::string>& out, int port)
+        {
+            if (m_disable_console_login)
+            {
+                jsonObject_t dataMap;
+                dataMap["BROWSER_MODE_REDIRECT_PORT"] = picojson::value((double)port);
+                if (!m_idp->getIDPInfo(dataMap)) {
+                    return;
+                }
+                out[std::string("LOGIN_URL")] = m_idp->ssoURLStr;
+                out[std::string("PROOF_KEY")] = m_idp->proofKey;
+            }
+            else
+            {
+                std::string proofKey = generateProofKey();
+                SFURL connectURL = m_idp->getServerURLSync().path("/console/login");
+                connectURL.addQueryParam("login_name", m_idp->m_user);
+                connectURL.addQueryParam("browser_mode_redirect_port", std::to_string(m_authWebServer->getPort()));
+                connectURL.addQueryParam("proof_key", proofKey);
+
+                m_idp->ssoURLStr = connectURL.toString();
+                out[std::string("LOGIN_URL")] = m_idp->ssoURLStr;
+                out[std::string("PROOF_KEY")] = proofKey;
+            }
+            CXX_LOG_DEBUG("sf", "AuthenticatorExternalBrowser", "getSS  OUrl",
+                "SSO URL: %s", m_idp->ssoURLStr.c_str());
+        }
+
+        std::string IAuthenticatorExternalBrowser::generateProofKey()
+        {
+            std::vector<char> randomness(32);
+            RAND_bytes(reinterpret_cast<unsigned char*>(randomness.data()), randomness.size());
+            return Base64::encodePadding(randomness);
+        }
+
+        /**
+         * Start web browser so that the user can type IdP user and password
+         * @param ssoUrl SSO URL
+         */
+        void IAuthenticatorExternalBrowser::startWebBrowser(std::string ssoUrl)
+        {
+            // Validate the SSO URL to mitigate OS command injection vulnerability.
+            // Semicolon (;) and single quote (') are not allowed.
+            char regexStr[] = "^http(s?)\\:\\/\\/[0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z@:])*(:(0-9)*)*(\\/?)([a-zA-Z0-9\\-\\.\\?\\,\\&\\(\\)\\/\\\\\\+&%\\$#_=@]*)?$";
+            if (!std::regex_match(ssoUrl, std::regex(regexStr)))
+            {
+                CXX_LOG_ERROR("sf", "AuthenticatorExternalBrowser", "startWebBrowser",
+                    "Failed to start web browser. %s", "Invalid SSO URL.");
+                m_errMsg = "SFAuthWebBrowserFailed: Invalid SSO URL";
+            }
+
+            std::cout << "Initiating login request with your identity provider. A "
+                "browser window should have opened for you to complete the "
+                "login. If you can't see it, check existing browser windows, "
+                "or your OS settings. Press CTRL+C to abort and try again..." << "\n";
+            // need double quotes to reserve ampasand characters
+            std::stringstream urlBuf;
+#ifdef __APPLE__
+            openURL(ssoUrl);
+#elif _WIN32
+            HINSTANCE ret = ShellExecuteA(NULL, "open", ssoUrl.c_str(), NULL, NULL,
+                SW_SHOWNORMAL);
+            if (ret > (HINSTANCE)32)
+            {
+                // success
+                return;
+            }
+            CXX_LOG_ERROR("sf", "AuthenticatorExternalBrowser", "startWebBrowser",
+                "Failed to start web browser. err: %d", (int)(unsigned long long)ret);
+            m_errMsg = "SFAuthWebBrowserFailed: Failed to start web browser";
+
+#else
+            // use fork to avoid using system() call and prevent command injection
+            char* argv[3];
+            pid_t child_pid;
+            int child_status;
+            argv[0] = const_cast<char*>("xdg-open");
+            argv[1] = const_cast<char*>(ssoUrl.c_str());
+            argv[2] = NULL;
+
+            child_pid = fork();
+            if (child_pid < 0)
+            {
+                // fork failed
+                CXX_LOG_ERROR("sf", "AuthenticatorExternalBrowser", "startWebBrowser",
+                    "Failed to start web browser on fork.");
+                m_errMsg = "SFAuthWebBrowserFailed: Failed to start web browser on fork";
+            }
+            else if (child_pid == 0)
+            {
+                // This is done by the child process.
+                execvp(argv[0], argv);
+                /* If execvp returns, it must have failed. */
+                exit(-1); // Do nothing as we are in child process
+            }
+            else
+            {
+                // This is run by the parent. Wait for the child to terminate.
+                if (waitpid(child_pid, &child_status, 0) < 0)
+                {
+                    CXX_LOG_ERROR("sf", "AuthenticatorExternalBrowser", "startWebBrowser",
+                        "Failed to start web browser on waitpid.");
+                    m_errMsg = "SFAuthWebBrowserFailed: Failed to start web browser on waitpid";
+                }
+
+                if (WIFEXITED(child_status)) {
+                    const int es = WEXITSTATUS(child_status);
+                    if (es != 0)
+                    {
+                        CXX_LOG_ERROR("sf", "AuthenticatorExternalBrowser", "startWebBrowser",
+                            "Failed to start web browser. xdg-open returned %d", es);
+                        m_errMsg = "SFAuthWebBrowserFailed: Failed to start web browser. xdg-open returned";
+                    }
+                }
+            }
+#endif
+        }
+
+#ifdef __APPLE__
+        void IAuthenticatorExternalBrowser::openURL(const std::string& url_str) {
+            CFURLRef url = CFURLCreateWithBytes(
+                NULL,                        // allocator
+                (UInt8*)url_str.c_str(),     // URLBytes
+                url_str.length(),            // length
+                kCFStringEncodingASCII,      // encoding
+                NULL                         // baseURL
+            );
+            LSOpenCFURLRef(url, 0);
+            CFRelease(url);
+        }
+#endif
+
+#ifdef _WIN32
+        AuthWinSock::AuthWinSock()
+        {
+            WORD wVersionRequested;
+            WSADATA wsaData;
+            int err;
+            wVersionRequested = MAKEWORD(2, 2);
+            err = WSAStartup(wVersionRequested, &wsaData);
+            if (err != 0)
+            {
+                CXX_LOG_ERROR(
+                    "sf", "AuthWinSock", "constructor",
+                    "Failed to call WSAStartup: %d", err);
+                m_errMsg = "SFAuthWebBrowserFailed: Failed to call WSAStartup";
+            }
+            if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+            {
+                /* Tell the user that we could not find a usable */
+                /* WinSock DLL.                                  */
+                WSACleanup();
+                CXX_LOG_ERROR(
+                    "sf", "AuthWinSock", "constructor",
+                    "Could not find a usable version of Winsock.dll.%s", "");
+                m_errMsg = "SFAuthWebBrowserFailed: Could not find a usable version of Winsock.dll";
+            }
+
+            CXX_LOG_INFO("sf", "AuthWinSock",
+                "constructor",
+                "Winsock %s.%s DLL was found",
+                std::to_string(LOBYTE(wsaData.wVersion)).c_str(),
+                std::to_string(HIBYTE(wsaData.wVersion)).c_str());
+        }
+
+        AuthWinSock::~AuthWinSock()
+        {
+            WSACleanup();
+        }
+#endif
 
         void IAuthenticatorOKTA::authenticate() 
         {
---
+++ tests/test_unit_external_browser.cpp
@@ -0,0 +1,500 @@
+﻿/*
+ * Copyright (c) 2024 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#include <string>
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include "snowflake/SFURL.hpp"
+#include "../lib/connection.h"
+#include "../lib/authenticator.h"
+#include "../cpp/lib/Authenticator.hpp"
+#include "utils/test_setup.h"
+#include "utils/TestSetup.hpp"
+#include "../cpp/logger/SFLogger.hpp"
+#ifdef _WIN32
+#include <WS2tcpip.h>
+#else
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+#define MOCK_GET_RESPONSE "GET /?token=Snowflake-token-12345 HTTP/1.1"
+#define MOCK_POST_RESPONSE "POST token=Snowflake-token-12345"
+
+#define REF_PORT 12345
+#define REF_SSO_URL "https://sso.com/"
+#define REF_PROOF_KEY "MOCK_PROOF_KEY"
+#define REF_SAML_TOKEN "MOCK_SAML_TOKEN"
+#define CACHE_SERVER_URL_ENV "SF_OCSP_RESPONSE_CACHE_SERVER_URL"
+
+using namespace Snowflake::Client;
+
+static bool endsWith(const std::string& a, const std::string& b) 
+{
+    if (b.size() > a.size()) return false;
+    return std::equal(a.begin() + a.size() - b.size(), a.end(), b.begin());
+}
+
+class MockAuthWebServer : public AuthWebServer
+{
+public:
+    MockAuthWebServer()
+    {}
+
+    virtual ~MockAuthWebServer()
+    {}
+
+    inline void start()
+    {}
+
+    inline void stop()
+    {}
+
+    inline int getPort()
+    {
+        return REF_PORT;
+    }
+
+    inline void startAccept()
+    {}
+
+    inline bool receive()
+    {
+        return false;
+    }
+
+    inline std::string getSAMLToken()
+    {
+        return std::string(REF_SAML_TOKEN);
+    }
+
+    inline bool isConsentCacheIdToken()
+    {
+        return true;
+    }
+
+    inline int getTimeout()
+    {
+        return m_timeout;
+    }
+};
+
+class MockIDP : public IDPAuthenticator
+{
+public:
+    MockIDP(SF_CONNECT* connection) : m_connection(connection)
+    {
+        m_account = m_connection->account;
+        m_authenticator = m_connection->authenticator;
+        m_user = m_connection->user;
+        m_port = m_connection->port;
+        m_host = m_connection->host;
+        m_protocol = m_connection->protocol;
+        m_retriedCount = 0;
+        m_retryTimeout = get_retry_timeout(m_connection);
+    }
+
+    ~MockIDP()
+    {
+
+    }
+
+    inline bool curlPostCall(SFURL& url, const jsonObject_t& obj, jsonObject_t& resp)
+    {
+        SF_UNUSED(url);
+        SF_UNUSED(obj);
+
+        bool ret = true;
+        jsonObject_t data;
+        data["tokenUrl"] = jsonValue_t("https://fake.okta.com/tokenurl");
+        data["ssoUrl"] = jsonValue_t("https://fake.okta.com/ssourl");
+        data["proofKey"] = jsonValue_t("MOCK_PROOF_KEY");
+        resp["data"] = jsonValue_t(data);
+        resp["sessionToken"] = jsonValue_t("onetimetoken");
+
+        //The curlPostCall is called twice in authenticator 1. getIDPInfo 2. get onetime token
+        //This code is to test the get onetime token failure
+
+        return ret;
+    }
+
+    inline bool curlGetCall(SFURL& url, jsonObject_t& resp, bool parseJSON, std::string& rawData, bool& isRetry)
+    {
+        SF_UNUSED(url);
+        SF_UNUSED(resp);
+        SF_UNUSED(parseJSON);
+        SF_UNUSED(rawData);
+        SF_UNUSED(isRetry);
+        return false;
+    };
+
+    std::string getTokenURL();
+    std::string getSSOURL();
+
+private:
+    SF_CONNECT* m_connection;
+};
+
+void test_external_browser_initialize(void**)
+{
+    SF_CONNECT* sf = snowflake_init();
+    snowflake_set_attribute(sf, SF_CON_ACCOUNT, "test_account");
+    snowflake_set_attribute(sf, SF_CON_USER, "test_user");
+    snowflake_set_attribute(sf, SF_CON_PASSWORD, "test_password");
+    snowflake_set_attribute(sf, SF_CON_HOST, "wronghost.com");
+    snowflake_set_attribute(sf, SF_CON_PORT, "443");
+    snowflake_set_attribute(sf, SF_CON_PROTOCOL, "https");
+    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "externalbrowser");
+    sf_bool disable_console_login = SF_BOOLEAN_TRUE;
+    snowflake_set_attribute(sf, SF_CON_DISABLE_CONSOLE_LOGIN, &disable_console_login);
+   
+    _snowflake_check_connection_parameters(sf);
+    auth_initialize(sf);
+    IAuthenticator* auth = static_cast<IAuthenticator*>(sf->auth_object);
+    std::string type = typeid(*auth).name();
+    assert_true(type.find("AuthenticatorExternalBrowser") != std::string::npos);
+
+    snowflake_term(sf);
+}
+
+void test_external_browser_authenticate(void**)
+{
+    class MockExternalBrowser : public AuthenticatorExternalBrowser
+    {
+    public:
+        MockExternalBrowser(
+            SF_CONNECT* connection, IAuthWebServer* authWebServer)
+            : AuthenticatorExternalBrowser(connection, authWebServer)
+        {
+            m_idp = new MockIDP(connection);
+        }
+
+        ~MockExternalBrowser()
+        {}
+
+        inline void startWebBrowser(std::string ssoUrl)
+        {
+            SF_UNUSED(ssoUrl);
+        }
+
+        inline void cleanError()
+        {
+            m_errMsg = "";
+        }
+
+        bool m_errortesting = false;
+    };
+
+    SF_CONNECT* sf = snowflake_init();
+    snowflake_set_attribute(sf, SF_CON_ACCOUNT, "test_account");
+    snowflake_set_attribute(sf, SF_CON_USER, "test_user");
+    snowflake_set_attribute(sf, SF_CON_PASSWORD, "test_password");
+    snowflake_set_attribute(sf, SF_CON_HOST, "wronghost.com");
+    snowflake_set_attribute(sf, SF_CON_PORT, "443");
+    snowflake_set_attribute(sf, SF_CON_PROTOCOL, "https");
+    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "externalbrowser");
+    int64 timeout = 50;
+    snowflake_set_attribute(sf, SF_CON_BROWSER_RESPONSE_TIMEOUT, &timeout);
+    sf_bool disable_console_login = SF_BOOLEAN_TRUE;
+    snowflake_set_attribute(sf, SF_CON_DISABLE_CONSOLE_LOGIN, &disable_console_login);
+
+    IAuthWebServer* authWebServer = new MockAuthWebServer();
+    MockExternalBrowser* authenticatorInstance = new MockExternalBrowser(sf, authWebServer);
+    authenticatorInstance->authenticate();
+
+    assert_true(authenticatorInstance->getPort() != 0);
+    jsonObject_t dataMap;
+    authenticatorInstance->updateDataMap(dataMap);
+
+    assert_string_equal(dataMap["PROOF_KEY"].get<std::string>().c_str(), "MOCK_PROOF_KEY");
+    assert_string_equal(dataMap["TOKEN"].get<std::string>().c_str(), "MOCK_SAML_TOKEN");
+    assert_string_equal(dataMap["AUTHENTICATOR"].get<std::string>().c_str(), SF_AUTHENTICATOR_EXTERNAL_BROWSER);
+    assert_int_equal(static_cast<MockAuthWebServer*>(authWebServer)->getTimeout(),50);
+
+    delete authenticatorInstance;
+        
+    disable_console_login = SF_BOOLEAN_FALSE;
+    snowflake_set_attribute(sf, SF_CON_DISABLE_CONSOLE_LOGIN, &disable_console_login);
+
+    authWebServer = new MockAuthWebServer();
+    authenticatorInstance = new MockExternalBrowser(sf, authWebServer);
+    authenticatorInstance->authenticate();
+
+    dataMap.clear();
+    authenticatorInstance->updateDataMap(dataMap);
+
+    assert_true(!dataMap["PROOF_KEY"].get<std::string>().empty());
+    assert_string_equal(dataMap["TOKEN"].get<std::string>().c_str(), "MOCK_SAML_TOKEN");
+    assert_string_equal(dataMap["AUTHENTICATOR"].get<std::string>().c_str(), SF_AUTHENTICATOR_EXTERNAL_BROWSER);
+
+    delete authenticatorInstance;
+
+    snowflake_term(sf);
+}
+
+void createMockClient(int port, const char* response) {
+#ifdef _WIN32
+    Sleep(2000);
+#else
+    std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(2000)));
+#endif
+
+    int mock_client = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if (connect(mock_client, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Mock server connection failed\n";
+        return;
+    }
+
+    send(mock_client, response, strlen(response), 0);
+#ifndef _WIN32
+     close(mock_client);
+#else
+     closesocket(mock_client);
+#endif
+}
+
+class MockExternalBrowser : public AuthenticatorExternalBrowser
+{
+public:
+    MockExternalBrowser(
+        SF_CONNECT* connection, IAuthWebServer* authWebServer)
+        : AuthenticatorExternalBrowser(connection, authWebServer)
+    {
+        m_idp = new MockIDP(connection);
+    }
+
+    ~MockExternalBrowser()
+    {}
+
+
+    inline void startWebBrowser(std::string ssoUrl)
+    {
+        SF_UNUSED(ssoUrl);
+        std::thread mockServerThread(createMockClient, getPort(), m_response.c_str());
+        mockServerThread.detach();
+    }
+
+    std::string m_response;
+};
+
+void test_auth_web_server_success(void**) 
+{
+    log_set_quiet(0);
+    SF_CONNECT* sf = snowflake_init();
+    snowflake_set_attribute(sf, SF_CON_ACCOUNT, "test_account");
+    snowflake_set_attribute(sf, SF_CON_USER, "test_user");
+    snowflake_set_attribute(sf, SF_CON_PASSWORD, "test_password");
+    snowflake_set_attribute(sf, SF_CON_HOST, "wronghost.com");
+    snowflake_set_attribute(sf, SF_CON_PORT, "443");
+    snowflake_set_attribute(sf, SF_CON_PROTOCOL, "https");
+    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "externalbrowser");
+
+    MockExternalBrowser* auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = MOCK_GET_RESPONSE;
+    auth->authenticate();
+    assert_int_equal(sf->error.error_code, SF_STATUS_SUCCESS);
+
+    jsonObject_t dataMap;
+    auth->updateDataMap(dataMap);
+
+    assert_string_equal(dataMap["TOKEN"].get<std::string>().c_str(), "Snowflake-token-12345");
+    assert_string_equal(dataMap["PROOF_KEY"].get<std::string>().c_str(), "MOCK_PROOF_KEY");
+    assert_string_equal(dataMap["AUTHENTICATOR"].get<std::string>().c_str(), SF_AUTHENTICATOR_EXTERNAL_BROWSER);
+
+    auth->m_response = MOCK_POST_RESPONSE;
+    auth->authenticate();
+    assert_int_equal(sf->error.error_code, SF_STATUS_SUCCESS);
+
+    dataMap.clear();
+    auth->updateDataMap(dataMap);
+    assert_string_equal(dataMap["TOKEN"].get<std::string>().c_str(), "Snowflake-token-12345");
+    assert_string_equal(dataMap["PROOF_KEY"].get<std::string>().c_str(), "MOCK_PROOF_KEY");
+    assert_string_equal(dataMap["AUTHENTICATOR"].get<std::string>().c_str(), SF_AUTHENTICATOR_EXTERNAL_BROWSER);
+
+    snowflake_term(sf);
+    log_set_quiet(1);
+
+}
+
+void test_auth_web_server_fail(void**)
+{
+    SF_CONNECT* sf = snowflake_init();
+    snowflake_set_attribute(sf, SF_CON_ACCOUNT, "test_account");
+    snowflake_set_attribute(sf, SF_CON_USER, "test_user");
+    snowflake_set_attribute(sf, SF_CON_PASSWORD, "test_password");
+    snowflake_set_attribute(sf, SF_CON_HOST, "wronghost.com");
+    snowflake_set_attribute(sf, SF_CON_PORT, "443");
+    snowflake_set_attribute(sf, SF_CON_PROTOCOL, "https");
+    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "externalbrowser");
+
+    MockExternalBrowser* auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = "wrong request";
+    auth->authenticate();
+    assert_string_equal(sf->error.msg, "SFAuthWebBrowserFailed: Not HTTP request.");
+    delete auth;
+
+    auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = "GET /?token=Snowflake-token-12345 HTTP/1.3";
+    auth->authenticate();
+    assert_string_equal(sf->error.msg, "AuthWebServer:parseAndRespondGetRequest:Not HTTP request.");
+    delete auth;
+
+    auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = "GET /!token=Snowflake-token-12345 HTTP/1.1";
+    auth->authenticate();
+    assert_string_equal(sf->error.msg, "AuthWebServer:parseAndRespondGetRequest:No token parameter is found.");
+    delete auth;
+
+    auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = "OPTIONS ";
+    auth->authenticate();
+    assert_string_equal(sf->error.msg, "AuthWebServer:parseAndRespondOptionsRequest:no Access-Control-Request-Headers or Origin header.");
+    delete auth;
+
+    auth = new MockExternalBrowser(sf, nullptr);
+    auth->m_response = "OPTIONS / api / data HTTP / 1.1 \n Host: api.example.com \n Origin : https ://client.example.com \nAccess-Control-Request-Method : GET";
+    auth->authenticate();
+    assert_string_equal(sf->error.msg, "AuthWebServer:parseAndRespondOptionsRequest:POST method is not requested.");
+    delete auth;
+
+    snowflake_term(sf);
+}
+
+void unit_authenticator_external_browser_privatelink(const std::string& topDomain = "com")
+{
+    class EnvVar
+    {
+    public:
+        EnvVar() : exist_(false)
+        {
+            char* cache_server_url_env = getenv(CACHE_SERVER_URL_ENV);
+            if (nullptr != cache_server_url_env) {
+                original_value_ = std::string(cache_server_url_env);
+                exist_ = true;
+            }
+        }
+        ~EnvVar()
+        {
+            if (exist_) {
+                sf_setenv(CACHE_SERVER_URL_ENV, original_value_.c_str());
+            }
+            else {
+                sf_unsetenv(CACHE_SERVER_URL_ENV);
+            }
+        }
+
+        static std::string get()
+        {
+            char* cache_server_url_env = getenv(CACHE_SERVER_URL_ENV);
+            const std::string cache_server_url(cache_server_url_env ? cache_server_url_env : "");
+            return std::string(cache_server_url_env ? cache_server_url_env : "");
+        }
+
+        static void validate(const std::string& topDomain = "com")
+        {
+            const std::string PRIVATELINK_URL_SUFFIX = std::string(".privatelink.snowflakecomputing.") + topDomain + "/ocsp_response_cache.json";
+            const std::string cache_server_url = get();
+            assert_true(endsWith(cache_server_url, PRIVATELINK_URL_SUFFIX));
+        }
+
+    private:
+        std::string original_value_;
+        bool exist_;
+
+    };
+
+    class MockAuthenticatorExternalBrowser : public AuthenticatorExternalBrowser
+    {
+    public:
+        MockAuthenticatorExternalBrowser(
+            SF_CONNECT* connection, IAuthWebServer* authWebServer, const std::string& topDomain = "com")
+            : AuthenticatorExternalBrowser(connection, authWebServer), m_topDomain(topDomain)
+        {}
+
+        inline void startWebBrowser(std::string ssoUrl)
+        {
+            assert_string_equal(ssoUrl.c_str(), REF_SSO_URL);
+        }
+
+        inline void getLoginUrl(std::map<std::string, std::string>& out, int port)
+        {
+            assert_int_equal(port, REF_PORT);
+            out[std::string("LOGIN_URL")] = std::string(REF_SSO_URL);
+            out[std::string("PROOF_KEY")] = std::string(REF_PROOF_KEY);
+        }
+
+        inline void authenticate()
+        {
+            EnvVar::validate(m_topDomain);
+            AuthenticatorExternalBrowser::authenticate();
+        }
+
+    private:
+        std::string m_topDomain;
+    };
+
+    // Helper class to show/validate environment variable SF_OCSP_RESPONSE_CACHE_SERVER_URL
+    // It also restore the original value after test.
+    // note: function is not allowed in a function scope, but a class is.
+    
+
+    EnvVar original_env_value; // load now and restore when test ends
+
+    SF_CONNECT* sf = snowflake_init();
+    snowflake_set_attribute(sf, SF_CON_ACCOUNT, "test_account");
+    snowflake_set_attribute(sf, SF_CON_USER, "test_user");
+    snowflake_set_attribute(sf, SF_CON_PASSWORD, "test_password");
+    std::string host = "testaccount.privatelink.snowflakecomputing." + topDomain;
+    snowflake_set_attribute(sf, SF_CON_HOST, host.c_str());
+    snowflake_set_attribute(sf, SF_CON_PORT, "443");
+    snowflake_set_attribute(sf, SF_CON_PROTOCOL, "https");
+    snowflake_set_attribute(sf, SF_CON_AUTHENTICATOR, "externalbrowser");
+    sf_bool disable_console_login = SF_BOOLEAN_TRUE;
+    snowflake_set_attribute(sf, SF_CON_DISABLE_CONSOLE_LOGIN, &disable_console_login);
+    _snowflake_check_connection_parameters(sf);
+
+    MockAuthWebServer* webSever = new MockAuthWebServer();
+    sf->auth_object = static_cast<Snowflake::Client::IAuthenticator*>(
+        new MockAuthenticatorExternalBrowser(sf, webSever, topDomain));
+    jsonObject_t dataMap;
+
+    static_cast<Snowflake::Client::IAuthenticator*>(sf->auth_object)->authenticate();
+
+    snowflake_term(sf);
+}
+
+void test_unit_authenticator_external_browser_privatelink(void**)
+{
+    unit_authenticator_external_browser_privatelink();
+
+}
+
+void test_authenticator_external_browser_privatelink_with_china_domain(void**)
+{
+    unit_authenticator_external_browser_privatelink("cn");
+}
+
+int main(void) {
+  initialize_test(SF_BOOLEAN_FALSE);
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_external_browser_initialize),
+    cmocka_unit_test(test_external_browser_authenticate),
+    cmocka_unit_test(test_auth_web_server_success),
+    cmocka_unit_test(test_auth_web_server_fail),
+    cmocka_unit_test(test_unit_authenticator_external_browser_privatelink),
+    cmocka_unit_test(test_authenticator_external_browser_privatelink_with_china_domain),
+  };
+  int ret = cmocka_run_group_tests(tests, NULL, NULL);
+  return ret;
+}
