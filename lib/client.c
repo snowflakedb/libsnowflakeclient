ecd2ec60bb579cdf6966dab0668a56629052fe9e
John Yun <jonghun.yun@insightsoftware.com>
2025-03-12 10:38:29 -0700

    add password condition

--- lib/client.c
+++ lib/client.c
@@ -623,7 +623,7 @@ _snowflake_check_connection_parameters(SF_CONNECT *sf) {
         return SF_STATUS_ERROR_GENERAL;
     }
 
-    if ((AUTH_JWT != auth_type) && (AUTH_OAUTH != auth_type) && (AUTH_PAT != auth_type) && (is_string_empty(sf->password))) {
+    if (is_password_required(auth_type) && (is_string_empty(sf->password))) {
         // Invalid password
         log_error(ERR_MSG_PASSWORD_PARAMETER_IS_MISSING);
         SET_SNOWFLAKE_ERROR(
