aa18f63c41a052a2dc6cf7232d08bfe5719989ea
gina_chang <gina.chang@insightsoftware.com>
2025-07-10 11:57:14 -0700

    [SNOW-856234] cleaned up test case

--- tests/test_unit_logger.c
+++ tests/test_unit_logger.c
@@ -5,8 +5,6 @@
 
 #include <string.h>
 #include <pwd.h>        // pwd structure
-// #include <sys/types.h>  //
-// #include <stdlib.h>     // set env
 #include <sys/stat.h>   //chmod and stat()
 
 #ifndef _WIN32
@@ -417,129 +415,27 @@ void test_client_config_stdout() {
 
 void test_log_creation_no_permission_to_home_folder(){
   // check if current user is root. If so, exit test
-  // FILE *mfptr;
-  // mfptr = fopen("easyLogging_testlog.txt", "w");
-  // fprintf(mfptr, "Getting logging info with pwd.h\n");
-
   char *name;
   struct passwd *pwd;
   pwd = getpwuid(getuid());
   name = pwd->pw_name;
-  
-  // fprintf(mfptr, "Login name: %s\n", name);
-
   assert_int_not_equal(strcmp(name, "root"), 0);
-  
-  // fprintf(mfptr, "Not root user; can test exception\n");
-  // fprintf(mfptr, "Creating Sf_client_config.json for user: %s\n", name);
-
-  // create sf_client_config.json in $HOME
-
-  // char clientConfigJSON[] = "{\"common\":{\"log_level\":\"warn\",\"log_path\":\"./test/\"}}";
-  char filename[] = "/sf_client_config.json";
+    
+  // retrieving HOME dir
   char *homedir = getenv("HOME");
-  size_t filepathsize = strlen(homedir)+strlen(filename)+1;
-  char *m_configFilePath = malloc(filepathsize);
-  snprintf(m_configFilePath, filepathsize, "%s%s", homedir, filename);
-
-  // fprintf(mfptr, "the file path: %s\n", m_configFilePath);
-
-  // FILE *file;
-  // file = fopen(m_configFilePath,"w");
-  // fprintf(file, "%s", clientConfigJSON);
-  // free(m_configFilePath);
-  // fclose(file);
   
-  // check home folder permission
-  // int rc = system("ls -l /home");
-  // if(rc == 0){
-    // fprintf(mfptr, "Terminal command: 'ls -l /home' executed successfully\n");
-  // } else {
-    // fprintf(mfptr, "Terminal command: 'ls -l /home' failed\n");
-  // }
-
+  // setting HOME dir to be inaccessible to all
   mode_t newPermission = 0000; // no read, write, execute permission for anyone
   chmod(homedir, newPermission);
-  // if(chmod(homedir, newPermission)== 0){
-  //   fprintf(mfptr, "Successfully changed HOME dir's permission\n");
-
-  //   struct stat st;
-  //   int returncode = stat(homedir, &st);
-  //   fprintf(mfptr, "%s Permissions: \t", homedir);
-  //   fprintf(mfptr, (S_ISDIR(st.st_mode)) ? "d" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IRUSR) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWUSR) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXUSR) ? "x" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IRGRP) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWGRP) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXGRP) ? "x" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IROTH) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWOTH) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXOTH) ? "x\n" : "-\n");
-
-  // } else {
-  //   fprintf(mfptr, "Failed in changing HOME dir permission\n");
-  // }
-
-  // rc = system("chmod 000 /home/test1");
-  // if(rc == 0){
-  //   fprintf(mfptr, "Terminal command: 'chmod 000 /home/test1' executed successfully\n");
-  // } else {
-  //   fprintf(mfptr, "Terminal command: 'chmod 000 /home/test1' failed\n");
-  // }
-
-  // set $HOME folder to root user
-  // fprintf(mfptr, "Current homedir: %s\n", homedir);
-  // if(setenv("HOME", "/root", 1) == 0){
-  //   homedir = getenv("HOME");
-  //   fprintf(mfptr, "Updated homedir: %s\n", homedir);
-  // } else {
-  //   fprintf(mfptr, "unable to set $HOME to /root\n");
-  // }
-
-
+  
   // parse client config for log details - exception should be thrown here and caught
   client_config clientConfig;
-  // fprintf(mfptr, "Attempting to load client config file from HOME\n");
   sf_bool result = load_client_config("", &clientConfig);
-  // fprintf(mfptr, "Successful in trying to load client config file from HOME\n");
   assert_false(result);
-
-  // // set global_client_config_attribute
-  // snowflake_global_set_attribute(SF_GLOBAL_CLIENT_CONFIG_FILE, m_configFilePath);
   
-  // // global_client_init  
-  // snowflake_global_init("./easy_logging", SF_LOG_DEFAULT, NULL);
-
-  // char LOG_PATH[MAX_PATH] = {0};
-  // // get log path determined by libsfclient -> should trigger exception
-  // snowflake_global_get_attribute(SF_GLOBAL_LOG_PATH, LOG_PATH, MAX_PATH);
-  // fprintf(mfptr, "Easy logging log path: %s\n", LOG_PATH);
-  
-  newPermission = 0755; // resetting HOME dir for the next test
+  // resetting HOME dir for the next test
+  newPermission = 0755; 
   chmod(homedir, newPermission);
-  // if(chmod(homedir, newPermission)== 0){
-  //   fprintf(mfptr, "Successfully changed HOME dir's permission\n");
-
-  //   struct stat st;
-  //   int returncode = stat(homedir, &st);
-  //   fprintf(mfptr, "%s Permissions: \t", homedir);
-  //   fprintf(mfptr, (S_ISDIR(st.st_mode)) ? "d" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IRUSR) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWUSR) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXUSR) ? "x" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IRGRP) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWGRP) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXGRP) ? "x" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IROTH) ? "r" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IWOTH) ? "w" : "-");
-  //   fprintf(mfptr, (st.st_mode & S_IXOTH) ? "x\n" : "-\n");
-
-  // } else {
-  //   fprintf(mfptr, "Failed in changing HOME dir permission\n");
-  // }
-  
-  // fclose(mfptr);
 }
 
 void test_log_creation() {
