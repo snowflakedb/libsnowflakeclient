94fe39529c6b65c59348aa0fdcd8b1b7f52a987c
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-09-03 15:51:31 +0200

    first version

--- tests/test_auth/test_runner.c
+++ tests/test_auth/test_runner.c
@@ -10,6 +10,8 @@ extern void test_okta_invalid_url_no_okta_path(void **state);
 extern void test_external_browser_successful_connection(void **state);
 extern void test_external_browser_mismatched_username(void **state);
 extern void test_external_browser_wrong_credentials(void **state);
+extern void test_mfa_totp_authentication(void **state);
+
 
 int main(void) {
     snowflake_global_init(NULL, SF_LOG_INFO, NULL);
@@ -24,7 +26,8 @@ int main(void) {
             cmocka_unit_test(test_okta_invalid_url_no_okta_path),
             cmocka_unit_test(test_external_browser_successful_connection),
             cmocka_unit_test(test_external_browser_mismatched_username),
-            cmocka_unit_test(test_external_browser_wrong_credentials)
+            cmocka_unit_test(test_external_browser_wrong_credentials),
+            cmocka_unit_test(test_mfa_totp_authentication)
     };
     int ret = cmocka_run_group_tests(tests, NULL, NULL);
     snowflake_global_term();
