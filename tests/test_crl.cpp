44dfa70b1ee0df957975379ca48250b69a715005
Dariusz Stempniak <dariusz.stempniak@snowflake.com>
2025-09-10 22:41:22 +0200

    SNOW-2117145 test

--- tests/test_crl.cpp
+++ tests/test_crl.cpp
@@ -131,47 +131,13 @@ void test_success_with_no_crl_in_advisory_mode(void **unused) {
   sf_unsetenv("SF_TEST_CRL_NO_CRL");
 }
 
-void test_fail_on_revoked_badssl(void **unused) {
-  SF_UNUSED(unused);
-
-  // we need to remove curl cache
-  ClientCurlDescPool::getInstance().init();
-  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(6000)));
-
-  // disable OCSP check
-  sf_bool value = SF_BOOLEAN_FALSE;
-  snowflake_global_set_attribute(SF_GLOBAL_OCSP_CHECK, &value);
-
-  sf_setenv("SNOWFLAKE_TEST_HOST", "revoked.badssl.com");
-
-  SF_CONNECT *sf = setup_snowflake_connection();
-
-  // enable CLR check
-  sf_bool crl_check = SF_BOOLEAN_TRUE;
-  snowflake_set_attribute(sf, SF_CON_CRL_CHECK, &crl_check);
-
-  SF_STATUS ret = snowflake_connect(sf);
-
-  // must fail with CURL error
-  assert_int_not_equal(ret, SF_STATUS_SUCCESS);
-  SF_ERROR_STRUCT *sferr = snowflake_error(sf);
-  if (sferr->error_code != SF_STATUS_ERROR_CURL) {
-    dump_error(sferr);
-  }
-  assert_int_equal(sferr->error_code, SF_STATUS_ERROR_CURL);
-  assert_string_equal(sferr->msg, "curl_easy_perform() failed: SSL peer certificate or SSH remote key was not OK");
-
-  snowflake_term(sf);
-}
-
 int main(void) {
     initialize_test(SF_BOOLEAN_FALSE);
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_succees_with_crl_check),
         cmocka_unit_test(test_fail_with_no_crl),
         cmocka_unit_test(test_success_with_no_crl_if_allow_no_crl),
-        cmocka_unit_test(test_success_with_no_crl_in_advisory_mode),
-        cmocka_unit_test(test_fail_on_revoked_badssl)
+        cmocka_unit_test(test_success_with_no_crl_in_advisory_mode)
     };
     int ret = cmocka_run_group_tests(tests, NULL, NULL);
     snowflake_global_term();
