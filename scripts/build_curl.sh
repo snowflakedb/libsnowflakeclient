a8e77e3decfbb1ec8af0f5364af7930255fdde7b
Michal Hofman <michal.hofman@snowflake.com>
2025-10-23 11:53:46 +0200

    SNOW-2117145 fix for mac build on jenkins

--- scripts/build_curl.sh
+++ scripts/build_curl.sh
@@ -107,7 +107,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         export CFLAGS="-arch arm64 -Xarch_arm64 -mmacosx-version-min=${MACOSX_VERSION_MIN}"
         export CPPFLAGS=-I$OOB_DEPENDENCY_DIR/include
         export LDFLAGS=-L$OOB_DEPENDENCY_DIR/lib
-        PKG_CONFIG="pkg-config -static" LIBS="-ltelemetry -ldl" ./configure ${curl_configure_opts[@]}
+        PKG_CONFIG="pkg-config -static" LIBS="-ltelemetry -ldl" ./configure ${curl_configure_opts[@]} --host=arm
         make > /dev/null
         echo "lipo -create $LIBCURL_BUILD_DIR/lib/libcurl.a ./lib/.libs/libcurl.a -output $LIBCURL_BUILD_DIR/lib/../libcurl.a"
         lipo -create $LIBCURL_BUILD_DIR/lib/libcurl.a ./lib/.libs/libcurl.a -output $LIBCURL_BUILD_DIR/lib/../libcurl.a
