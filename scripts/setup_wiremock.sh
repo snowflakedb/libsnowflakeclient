56fbab3baad2d5162873541de12c4703a202d482
John Yun <jonghun.yun@insightsoftware.com>
2025-10-31 15:58:27 -0700

    upgrade java command

--- scripts/setup_wiremock.sh
+++ scripts/setup_wiremock.sh
@@ -21,3 +21,18 @@ chmod +r "$WIREMOCK_JAR"
 echo "SUCCESS: WireMock JAR (${WIREMOCK_VERSION}) downloaded successfully."
 echo "   - location: $WIREMOCK_JAR"
 echo "   - size: ${FILE_SIZE} bytes"
+
+JAVA_URL="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.12%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.12_7.tar.gz"
+INSTALL_DIR="${HOME}/.java17"
+DOWNLOAD_DIR="${HOME}/java17.tar.gz"
+
+mkdir -p $INSTALL_DIR
+
+# curl로 다운로드
+curl -L --fail $JAVA_URL -o "${DOWNLOAD_DIR}"
+
+# 압축 해제 및 설치 디렉토리로 이동 (폴더 구조에 따라 --strip-components=1 필요)
+tar -xzf "${DOWNLOAD_DIR}" -C $INSTALL_DIR --strip-components=1
+
+# 임시 파일 삭제
+rm "${DOWNLOAD_DIR}"
