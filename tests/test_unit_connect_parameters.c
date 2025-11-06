ef19058c55188cc4b9097a41f493254a318976f8
gina_chang <gina.chang@insightsoftware.com>
2025-11-06 14:45:05 -0800

    [SNOW-2316658] removed leading spaces that were added in

--- tests/test_unit_connect_parameters.c
+++ tests/test_unit_connect_parameters.c
@@ -3,7 +3,6 @@
 #include "utils/test_setup.h"
 #include "memory.h"
 
-#include <stdio.h>
 /**
  * Test default port
  */
@@ -282,17 +281,17 @@ void test_connection_parameters_including_cn_region(void **unused) {
 int main(void) {
     initialize_test(SF_BOOLEAN_FALSE);
     const struct CMUnitTest tests[] = {
-         cmocka_unit_test(test_connection_parameters_default_port),
-         cmocka_unit_test(test_connection_parameters_no_host),
-         cmocka_unit_test(test_connection_parameters_with_region),
-         cmocka_unit_test(test_connection_parameters_with_cn_region),
-         cmocka_unit_test(test_connection_parameters_including_region),
-         cmocka_unit_test(test_connection_parameters_including_region_including_dot),
-         cmocka_unit_test(test_connection_parameters_for_global_url_basic),
-         cmocka_unit_test(test_connection_parameters_for_global_url_full),
-         cmocka_unit_test(test_connection_parameters_application),
-         cmocka_unit_test(test_connect_with_renew),
-         cmocka_unit_test(test_connection_parameters_including_cn_region),
+        cmocka_unit_test(test_connection_parameters_default_port),
+        cmocka_unit_test(test_connection_parameters_no_host),
+        cmocka_unit_test(test_connection_parameters_with_region),
+        cmocka_unit_test(test_connection_parameters_with_cn_region),
+        cmocka_unit_test(test_connection_parameters_including_region),
+        cmocka_unit_test(test_connection_parameters_including_region_including_dot),
+        cmocka_unit_test(test_connection_parameters_for_global_url_basic),
+        cmocka_unit_test(test_connection_parameters_for_global_url_full),
+        cmocka_unit_test(test_connection_parameters_application),
+        cmocka_unit_test(test_connect_with_renew),
+        cmocka_unit_test(test_connection_parameters_including_cn_region),
     };
     int ret = cmocka_run_group_tests(tests, NULL, NULL);
     return ret;
