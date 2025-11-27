747ced354ed2b7380b03f9abe457489baaae6ba1
Harry Xi <harry.xi@insightsoftware.com>
2025-11-27 15:56:01 -0800

    SNOW-1997074 handle HTTP 307&308 redirect (#941)

--- lib/client.c
+++ lib/client.c
@@ -1046,7 +1046,7 @@ SF_CONNECT *STDCALL snowflake_init() {
         sf->token = NULL;
         sf->master_token = NULL;
         sf->login_timeout = SF_LOGIN_TIMEOUT;
-        sf->network_timeout = 0;
+        sf->network_timeout = SF_NETWORK_TIMEOUT;
         sf->browser_response_timeout = SF_BROWSER_RESPONSE_TIMEOUT;
         sf->retry_timeout = SF_RETRY_TIMEOUT;
         sf->sequence_counter = 0;
@@ -1522,7 +1522,7 @@ SF_STATUS STDCALL snowflake_set_attribute(
             sf->login_timeout = value ? *((int64 *) value) : SF_LOGIN_TIMEOUT;
             break;
         case SF_CON_NETWORK_TIMEOUT:
-            sf->network_timeout = value ? *((int64 *) value) : SF_LOGIN_TIMEOUT;
+            sf->network_timeout = value ? *((int64 *) value) : SF_NETWORK_TIMEOUT;
             break;
         case SF_CON_BROWSER_RESPONSE_TIMEOUT:
             sf->browser_response_timeout = value ? *((int64*)value) : SF_BROWSER_RESPONSE_TIMEOUT;
