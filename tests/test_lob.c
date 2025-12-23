de107bcefe20003cf13b295ed448b00b0350ded8
Harry Xi <harry.xi@insightsoftware.com>
2025-12-23 12:48:54 -0800

    shorten lob test

--- tests/test_lob.c
+++ tests/test_lob.c
@@ -107,7 +107,7 @@ void test_lob_setup(SF_CONNECT **out_sf, SF_STMT **out_sfstmt, sf_bool use_arrow
   // lower the timeout to speed up test and ignore possible timeout
   int64 timeout = 90;
   snowflake_set_attribute(sf, SF_CON_NETWORK_TIMEOUT, &timeout);
-  snowflake_set_attribute(sf, SF_CON_RETRY_TIMEOUT, &timeout);
+  sf->retry_timeout = timeout;
   SF_STATUS status = snowflake_connect(sf);
   if (status != SF_STATUS_SUCCESS) {
     dump_error(&(sf->error));
