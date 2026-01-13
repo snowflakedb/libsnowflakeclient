e13893ef8acecbe6c90819cbfb260c29bd845c21
Devansh Das <devansh.das@snowflake.com>
2026-01-13 12:37:01 +0100

    fix: Remove requirement of username during WIF auth in C interface

--- lib/client.c
+++ lib/client.c
@@ -627,7 +627,9 @@ _snowflake_check_connection_parameters(SF_CONNECT *sf) {
         return SF_STATUS_ERROR_GENERAL;
     }
 
-    if (!(auth_type == AUTH_EXTERNALBROWSER && sf->disable_console_login) && is_string_empty(sf->user)) {
+    if (!(auth_type == AUTH_EXTERNALBROWSER && sf->disable_console_login) && 
+        auth_type != AUTH_WIF && 
+        is_string_empty(sf->user)) {
         // Invalid user name
         log_error(ERR_MSG_USER_PARAMETER_IS_MISSING);
         SET_SNOWFLAKE_ERROR(
