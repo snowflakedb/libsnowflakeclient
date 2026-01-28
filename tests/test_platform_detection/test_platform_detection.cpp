27aab5729a1e6ab4901d29dccda17fae7cf48f90
Harry Xi <harry.xi@insightsoftware.com>
2026-01-27 17:02:50 -0800

    fix

--- tests/test_platform_detection/test_platform_detection.cpp
+++ tests/test_platform_detection/test_platform_detection.cpp
@@ -1,12 +1,19 @@
+#include <unistd.h>
 #include "snowflake/PlatformDetection.hpp"
 #include "../utils/test_setup.h"
 #include "../utils/TestSetup.hpp"
 #include "../wiremock/wiremock.hpp"
 
-using namespace Snowflake::Client;
-using namespace Snowflake::Client::PlatformDetection;
+extern "C"
+{
+extern void resetDetection();
+extern void redirectMetadataBaseUrl(const char* url);
+extern void restoreMetadataBaseUrl();
+}
 
-Snowflake::Client::WiremockRunner* wiremock = NULL;
+namespace Snowflake::Client {
+
+WiremockRunner* wiremock = NULL;
 // Check if WireMock is running by polling the admin endpoint
 static bool isWiremockRunning() {
   const std::string request = std::string("curl -s --output /dev/null -X POST http://") +
@@ -44,13 +51,6 @@ int teardown_wiremock(void **) {
   return 0;
 }
 
-extern "C"
-{
-extern void resetDetection();
-extern void redirectMetadataBaseUrl(const char* url);
-extern void restoreMetadataBaseUrl();
-}
-
 void test_detection_endpoint_core(const std::string& expectedPlatform, const std::string& mappingFile)
 {
   resetDetection();
@@ -59,7 +59,7 @@ void test_detection_endpoint_core(const std::string& expectedPlatform, const std
   std::string wiremockUrl = std::string("http://") + wiremockHost + ":" + wiremockAdminPort;
   redirectMetadataBaseUrl(wiremockUrl.c_str());
   std::vector<std::string> detectedPlatforms;
-  getDetectedPlatforms(detectedPlatforms);
+  PlatformDetection::getDetectedPlatforms(detectedPlatforms);
   assert_int_equal(1, detectedPlatforms.size());
   assert_string_equal(detectedPlatforms[0].c_str(), expectedPlatform.c_str());
   restoreMetadataBaseUrl();
@@ -69,6 +69,9 @@ void test_ec2Instance(void**) {
   test_detection_endpoint_core("is_ec2_instance", "aws_ec2_instance_success.json");
 }
 
+} // namespace Snowflake::Client
+
+using namespace Snowflake::Client;
 int main(void) {
   initialize_test(SF_BOOLEAN_FALSE);
   const struct CMUnitTest tests[] = {
