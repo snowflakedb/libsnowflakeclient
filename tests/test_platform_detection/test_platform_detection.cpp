1489ff34cffdc24d690e2bdeb0be7855f1a04181
Harry Xi <harry.xi@insightsoftware.com>
2026-01-27 16:24:56 -0800

    fix failure

--- tests/test_platform_detection/test_platform_detection.cpp
+++ tests/test_platform_detection/test_platform_detection.cpp
@@ -7,26 +7,29 @@ using namespace Snowflake::Client;
 
 Snowflake::Client::WiremockRunner* wiremock = NULL;
 
+extern "C"
+{
 extern void resetDetection();
 extern void redirectMetadataBaseUrl(const char* url);
 extern void restoreMetadataBaseUrl();
+}
 
 void test_detection_endpoint_core(const std::string& expectedPlatform, const std::string& mappingFile)
 {
   resetDetection();
+  delete(wiremock);
   wiremock = new WiremockRunner(mappingFile, {});
-  std::string wiremockUrl = std::string("http://") + wiremockHost + ":" + wiremockPort;
+  std::string wiremockUrl = std::string("http://") + wiremockHost + ":" + wiremockAdminPort;
   redirectMetadataBaseUrl(wiremockUrl.c_str());
   std::vector<std::string> detectedPlatforms;
   getDetectedPlatforms(detectedPlatforms);
   assert_int_equal(1, detectedPlatforms.size());
   assert_string_equal(detectedPlatforms[0].c_str(), expectedPlatform.c_str());
   restoreMetadataBaseUrl();
-  delete(wiremock);
 }
 
 void test_ec2Instance(void**) {
-  test_detection_endpoint_core("is_ec2_instance", "aws_ec2_instance_success");
+  test_detection_endpoint_core("is_ec2_instance", "aws_ec2_instance_success.json");
 }
 
 int main(void) {
