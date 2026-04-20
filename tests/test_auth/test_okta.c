b152ff03808361f9e2d457169cfe8d9dbc61f549
Piotr Bulawa <piotr.bulawa@snowflake.com>
2026-04-20 23:18:16 +0200

    Fix assertions

--- tests/test_auth/test_okta.c
+++ tests/test_auth/test_okta.c
@@ -43,7 +43,7 @@ void test_okta_invalid_url(void **unused) {
     SF_STATUS status = snowflake_connect(sf);
     assert_int_equal(status, SF_STATUS_ERROR_GENERAL);
     SF_ERROR_STRUCT* error = snowflake_error(sf);
-    assert_string_equal(error->msg, "Authentication failed");
+    assert_string_equal(error->msg, "authentication failed");
     snowflake_term(sf);
 }
 
@@ -59,6 +59,6 @@ void test_okta_invalid_url_no_okta_path(void **unused) {
     SF_STATUS status = snowflake_connect(sf);
     assert_int_equal(status, SF_STATUS_ERROR_GENERAL);
     SF_ERROR_STRUCT* error = snowflake_error(sf);
-    assert_string_equal(error->msg, "Authentication failed");
+    assert_string_equal(error->msg, "authentication failed");
     snowflake_term(sf);
 }
