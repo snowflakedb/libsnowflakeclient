892cc2bf3d7313ea5fcfd6a388289890098d7507
sfc-gh-ext-simba-lf <louie.farol@insightsoftware.com>
2026-07-08 17:35:37 -0700

    Update curl to 8.21.0

--- scripts/_init.sh
+++ scripts/_init.sh
@@ -14,7 +14,7 @@ mkdir -p $ARTIFACTS_DIR
 
 PLATFORM=$(echo $(uname) | tr '[:upper:]' '[:lower:]')
 CJSON_VERSION=1.7.19
-CURL_VERSION=8.20.0
+CURL_VERSION=8.21.0
 
 # Find cmake, gcc and g++ on target machine. Need cmake 3.0+, gcc/g++ 4.9+
 if which cmake3 >& /dev/null; then
