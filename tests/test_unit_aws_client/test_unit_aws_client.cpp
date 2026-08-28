6de9394afa231cfddeace7f56cd010f5d8011634
sfc-gh-ext-simba-hx <harry.xi@insightsoftware.com>
2026-08-28 15:48:26 -0700

    update test case

--- tests/test_unit_aws_client/test_unit_aws_client.cpp
+++ tests/test_unit_aws_client/test_unit_aws_client.cpp
@@ -1,10 +1,13 @@
+#include <fstream>
 #include "../wiremock/wiremock.hpp"
-
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include "snowflake/client.h"
-#include "snowflake/secure_storage.h"
+#include "snowflake/IStatementPutGet.hpp"
+#include "snowflake/PutGetParseResponse.hpp"
+#include <snowflake/SnowflakeTransferException.hpp>
+#include "util/Base64.hpp"
+#include "FileTransferExecutionResult.hpp"
+#include "FileTransferAgent.hpp"
+#include "SnowflakeS3Client.hpp"
+#include "StorageClientFactory.hpp"
 #include "../utils/test_setup.h"
 #include "../utils/TestSetup.hpp"
 
@@ -12,26 +15,85 @@ using namespace Snowflake::Client;
 
 static int group_setup(void**)
 {
-    return 0;
+  return 0;
 }
 
 static int group_teardown(void**)
 {
-    return 0;
+  return 0;
 }
+
+class MockedStatementPutGet : public Snowflake::Client::IStatementPutGet
+{
+public:
+  MockedStatementPutGet()
+  {
+  }
+
+  bool parsePutGetCommand(std::string *sql,
+      PutGetParseResponse *putGetParseResponse)
+  {
+    return true;
+  }
+};
 
 void test_retry_parts_uploading(void** unused)
 {
-    SF_UNUSED(unused);
+  SF_UNUSED(unused);
+  TransferConfig transferConfig;
+  memset(&transferConfig, 0, sizeof(transferConfig));
+  char cafile[] = "/tmp/cafile";
+  transferConfig.caBundleFile = cafile;
+  StageInfo stageInfo;
+  stageInfo.stageType = StageType::S3;
+  stageInfo.endPoint = "s3.us-east-1.amazonaws.com";
+  char aws_token[] = "AWS_TOKEN";
+  char aws_key_id[] = "AWS_KEY_ID";
+  char aws_secret_key[] = "AWS_SECRET_KEY";
+  stageInfo.credentials.insert( {{"AWS_TOKEN", aws_token},
+                                   {"AWS_KEY_ID", aws_key_id},
+                                   {"AWS_SECRET_KEY", aws_secret_key}});
 
+  std::string datafile = "../data/large_file.csv.gz";
+  struct stat fileStatus;
+  stat(datafile.c_str(), &fileStatus);
+  size_t filesize = (size_t) fileStatus.st_size;
+  std::fstream dataStream(datafile.c_str(), ::std::ios_base::in | ::std::ios_base::binary);
+  if (!dataStream.is_open())
+  {
+    std::cerr << "Failed to open file!" << std::endl;
+    return;
+  }
+
+  FileMetadata fileMetadata;
+  fileMetadata.srcFileName = datafile;
+  fileMetadata.srcFileToUpload = datafile;
+  fileMetadata.requireCompress = false;
+  fileMetadata.srcFileSize = filesize;
+  fileMetadata.srcFileToUploadSize = filesize;
+  fileMetadata.sha256Digest = "dummydigest";
+  fileMetadata.destFileName = "dummydst.csv.gz";
+  fileMetadata.encryptionMetadata.cipherStreamSize = (long long)filesize;
+  fileMetadata.destFileSize = filesize;
+  fileMetadata.encryptionMetadata.fileKey.nbBits = 0;
+  fileMetadata.sourceCompression = &FileCompressionType::GZIP;
+  fileMetadata.targetCompression = &FileCompressionType::GZIP;
+  fileMetadata.isLarge = true;
+  fileMetadata.overWrite = true;
+
+  MockedStatementPutGet mockedStatement;
+  SnowflakeS3Client s3Client(&stageInfo, 2, 40 * 1024 * 1024, &transferConfig, &mockedStatement);
+  s3Client.upload(&fileMetadata, (std::basic_iostream<char>*)&dataStream);
+
+  dataStream.close();
 }
 
 int main(void) {
-    initialize_test(SF_BOOLEAN_FALSE);
-    const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_retry_parts_uploading),
-    };
-    int ret = cmocka_run_group_tests(tests, group_setup, group_teardown);
-    snowflake_global_term();
-    return ret;
+  initialize_test(SF_BOOLEAN_FALSE);
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_retry_parts_uploading),
+  };
+  int ret = cmocka_run_group_tests(tests, group_setup, group_teardown);
+  snowflake_global_term();
+  return ret;
 }
