d5614f454feec5c421a088bfea56113317e5b358
John Yun <140559986+sfc-gh-ext-simba-jy@users.noreply.github.com>
2026-09-22 09:08:09 -0700

    Refactor build script for Azure SDK
    Updated azure_dir variable and modified build directory path.

--- scripts/build_azuresdk.bat
+++ scripts/build_azuresdk.bat
@@ -15,7 +15,7 @@ goto :EOF
 
 :build
 setlocal
-set azure_dir=azure-sdk-for-cpp
+set azure_dir=a
 set platform=%1
 set build_type=%2
 set vs_version=%3
@@ -44,7 +44,7 @@ if "%dynamic_runtime%"=="OFF" (
 
 @echo off
 set AZURE_SOURCE_DIR=%scriptdir%..\deps\%azure_dir%
-set AZURE_CMAKE_BUILD_DIR=%AZURE_SOURCE_DIR%\cmake-build-%arcdir%-%vs_version%-%build_type%
+set AZURE_CMAKE_BUILD_DIR=%AZURE_SOURCE_DIR%\%arcdir%-%build_type%
 set AZURE_INSTALL_DIR=%scriptdir%..\deps-build\%build_dir%\azure
 
 rd /S /Q %AZURE_SOURCE_DIR%
